6bc5abc7e59361b5efa566404d4f6a5ecc6aaa31
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-11 23:35:31 -0500

    added Direction enum

--- src/main/java/hm/zelha/particlesfx/particles/parents/Directional.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/Directional.java
@@ -7,4 +7,27 @@ public interface Directional {
     public void setDirection(BlockFace face);
 
     public BlockFace getDirection();
+
+    /** while it is possible to get more directions than this, it's only possible to get variants of north west. pretty weird. */
+    enum Direction {
+        NORTH(1),
+        SOUTH(7),
+        EAST(5),
+        WEST(3),
+        NORTH_EAST(2),
+        NORTH_WEST(0),
+        SOUTH_EAST(8),
+        SOUTH_WEST(6),
+        NONE(4);
+
+        private final int value;
+
+        Direction(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
 }
