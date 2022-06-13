8e16fd50c886e5b724caa687d33307560bd394f9
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-12 19:02:29 -0500

    removed unnecessary constructors and updated javadocs

--- src/main/java/hm/zelha/particlesfx/particles/ParticleSmokeDirectional.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleSmokeDirectional.java
@@ -5,43 +5,27 @@ import hm.zelha.particlesfx.particles.parents.Particle;
 import org.bukkit.Effect;
 
 /**
- * sadly, the directional version of the smoke particle is locked to the center of whatever block it's played on
+ * since Effect.SMOKE is Type.VISUAL, the radius, speed, and offsets are unused internally, and the default radius is quite small.
  * <p></p>
- * since Effect.SMOKE is Type.VISUAL, the radius and speed is fixed, and the radius is quite small.
+ * Type.VISUAL effects are also locked to specific coordinates of the block they're played on because their internal system uses BlockPosition
  */
 public class ParticleSmokeDirectional extends Particle implements Directional {
-    public ParticleSmokeDirectional(Direction direction, double offsetX, double offsetY, double offsetZ, int count) {
-        super(Effect.SMOKE, offsetX, offsetY, offsetZ, 0, count, 64);
+    public ParticleSmokeDirectional(Direction direction, int count) {
+        super(Effect.SMOKE, 0, 0, 0, 0, count, 64);
 
         super.data = direction;
     }
 
-    public ParticleSmokeDirectional(Direction direction, double offsetX, double offsetY, double offsetZ) {
-        this(direction, offsetX, offsetY, offsetZ, 1);
-    }
-
-    public ParticleSmokeDirectional(double offsetX, double offsetY, double offsetZ, int count) {
-        this(Direction.NONE, offsetX, offsetY, offsetZ, count);
-    }
-
-    public ParticleSmokeDirectional(double offsetX, double offsetY, double offsetZ) {
-        this(Direction.NONE, offsetX, offsetY, offsetZ, 1);
-    }
-
-    public ParticleSmokeDirectional(Direction direction, int count) {
-        this(direction, 0, 0, 0, count);
-    }
-
     public ParticleSmokeDirectional(Direction direction) {
-        this(direction, 0, 0, 0, 1);
+        this(direction, 1);
     }
 
     public ParticleSmokeDirectional(int count) {
-        this(Direction.NONE, 0, 0, 0, count);
+        this(Direction.NONE, count);
     }
 
     public ParticleSmokeDirectional() {
-        this(Direction.NONE, 0, 0, 0, 1);
+        this(Direction.NONE, 1);
     }
 
     @Override
