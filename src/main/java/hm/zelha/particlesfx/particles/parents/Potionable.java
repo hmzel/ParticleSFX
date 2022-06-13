66103af6d31fc37e198652dc4a2bbe6af7111394
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-12 21:49:55 -0500

    added parent class Potionable

---
+++ src/main/java/hm/zelha/particlesfx/particles/parents/Potionable.java
@@ -0,0 +1,10 @@
+package hm.zelha.particlesfx.particles.parents;
+
+import org.bukkit.potion.PotionType;
+
+public interface Potionable {
+
+    public void setPotionType(PotionType potion);
+
+    public PotionType getPotionType();
+}
