f1c146f07780e81c2366c8131025bbd740f0e63b
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-12 22:54:53 -0500

    added ParticleMobspawnerFlames

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleMobspawnerFlames.java
@@ -0,0 +1,19 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import org.bukkit.Effect;
+
+/**
+ * since Effect.MOBSPAWNER_FLAMES is Type.VISUAL, the radius, speed, and offsets are unused internally, and the default radius is quite small.
+ * <p></p>
+ * Type.VISUAL effects are also locked to specific coordinates of the block they're played on because their internal system uses BlockPosition
+ */
+public class ParticleMobspawnerFlames extends Particle {
+    public ParticleMobspawnerFlames(int count) {
+        super(Effect.MOBSPAWNER_FLAMES, 0, 0, 0, 0, count, 64);
+    }
+
+    public ParticleMobspawnerFlames() {
+        super(Effect.MOBSPAWNER_FLAMES, 0, 0, 0, 0, 1, 64);
+    }
+}
