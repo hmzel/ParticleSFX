55f005b465773b1f4147d463af6a5a2d136b9b44
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-14 18:20:41 -0500

    added enum NoteColor to NoteParticle and made NoteParticle's method use NoteColor

--- src/main/java/hm/zelha/particlesfx/particles/parents/NoteParticle.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/NoteParticle.java
@@ -2,7 +2,44 @@ package hm.zelha.particlesfx.particles.parents;
 
 public interface NoteParticle {
 
-    public void setNote(int note);
+    public void setNoteColor(NoteColor color);
 
-    public int getNote();
+    public NoteColor getNoteColor();
+
+    enum NoteColor {
+        CYAN(0),
+        PURPLE(4),
+        PURPLE_TWO(16),
+        RED(6),
+        YELLOW(7),
+        GOLD(8),
+        BROWN(9),
+        LIGHT_BLUE(12),
+        BLUE(14),
+        GREENISH_YELLOW(21),
+        GREEN(20),
+        DESATURATED_CYAN(1),
+        DESATURATED_PURPLE(3),
+        DESATURATED_PURPLE_TWO(17),
+        DESATURATED_LIGHT_BLUE(11),
+        DESATURATED_GREEN(19),
+        SATURATED_CYAN(23),
+        SATURATED_PURPLE(5),
+        SATURATED_PURPLE_TWO(15),
+        SATURATED_LIGHT_BLUE(13),
+        SATURATED_GREEN(22),
+        GRAY(10),
+        GRAY_TWO(2),
+        GRAY_THREE(18);
+
+        private final int value;
+
+        NoteColor(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
 }
