8aeae40d3c09a37ea725d5c00f0585290c21bf50
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-13 23:59:14 -0500

    added javadocs and annotations

--- src/main/java/hm/zelha/particlesfx/particles/parents/ColorableParticle.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/ColorableParticle.java
@@ -2,11 +2,16 @@ package hm.zelha.particlesfx.particles.parents;
 
 import org.bukkit.Color;
 
+import javax.annotation.Nullable;
+
 public interface ColorableParticle {
 
-    public void setColor(Color color);
+    /** @param color color to set, null if you want random coloring */
+    public void setColor(@Nullable Color color);
 
     public void setColor(int red, int green, int blue);
 
+    /** nullable to allow for randomly colored particles without use of boolean constructors*/
+    @Nullable
     public Color getColor();
 }
