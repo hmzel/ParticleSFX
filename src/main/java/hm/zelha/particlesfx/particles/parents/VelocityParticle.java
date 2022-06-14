4b0ef14e9d0286b74083edcf05c7adc0b7e19872
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-13 22:04:17 -0500

    updated javadocs

--- src/main/java/hm/zelha/particlesfx/particles/parents/VelocityParticle.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/VelocityParticle.java
@@ -9,16 +9,22 @@ import javax.annotation.Nullable;
 public interface VelocityParticle {
 
     /**
-     * since particle velocity is very volatile, the given velocity is automatically multiplied by 0.05 in all default implementations of
+     * since particle velocity is very volatile, the given velocity is automatically multiplied by a decimal in all default implementations of
      * VelocityParticle to prevent new users from setting the velocity to 1, 1, 1 and watching the particle fly into the sun. : )
+     * <p></p>
+     * said decimal makes sure that every VelocityParticle implementation follows the same convention,
+     * such that a Vector with x,y,z at 1 would make the particle move 1 block in all 3 axis on average.
      *
      * @param vector velocity to set
      */
     public void setVelocity(Vector vector);
 
     /**
-     * since particle velocity is very volatile, the given velocity is automatically multiplied by 0.05 in all default implementations of
+     * since particle velocity is very volatile, the given velocity is automatically multiplied by a decimal in all default implementations of
      * VelocityParticle to prevent new users from setting the velocity to 1, 1, 1 and watching the particle fly into the sun. : )
+     * <p></p>
+     * said decimal makes sure that every VelocityParticle implementation follows the same convention,
+     * such that a Vector with x,y,z at 1 would make the particle move 1 block in all 3 axis on average.
      *
      * @param x x velocity
      * @param y y velocity
