7b4d1ef28c231db77e2ea551e8f1f6b24928592d
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-15 18:47:39 -0500

    implimented new InverseTravellingParticle methods

--- src/main/java/hm/zelha/particlesfx/particles/ParticleGlyph.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleGlyph.java
@@ -1,16 +1,18 @@
 package hm.zelha.particlesfx.particles;
 
 import hm.zelha.particlesfx.particles.parents.Particle;
-import hm.zelha.particlesfx.particles.parents.TravellingParticle;
+import hm.zelha.particlesfx.particles.parents.InverseTravellingParticle;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import javax.annotation.Nullable;
 
-public class ParticleGlyph extends Particle implements TravellingParticle {
+public class ParticleGlyph extends Particle implements InverseTravellingParticle {
 
     private Location toGo;
+    private Vector velocity;
 
     public ParticleGlyph(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
         super(Effect.FLYING_GLYPH, offsetX, offsetY, offsetZ, 0, count, 0);
@@ -18,62 +20,128 @@ public class ParticleGlyph extends Particle implements TravellingParticle {
         this.toGo = toGo;
     }
 
+    public ParticleGlyph(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+        super(Effect.FLYING_GLYPH, offsetX, offsetY, offsetZ, 0, count, 0);
+
+        this.velocity = velocity;
+    }
+
     public ParticleGlyph(Location toGo, double offsetX, double offsetY, double offsetZ) {
         this(toGo, offsetX, offsetY, offsetZ, 1);
     }
 
+    public ParticleGlyph(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+        this(velocity, offsetX, offsetY, offsetZ, 1);
+    }
+
     public ParticleGlyph(double offsetX, double offsetY, double offsetZ, int count) {
-        this(null, offsetX, offsetY, offsetZ, count);
+        this((Location) null, offsetX, offsetY, offsetZ, count);
     }
 
     public ParticleGlyph(double offsetX, double offsetY, double offsetZ) {
-        this(null, offsetX, offsetY, offsetZ, 1);
+        this((Location) null, offsetX, offsetY, offsetZ, 1);
     }
 
     public ParticleGlyph(Location toGo, int count) {
         this(toGo, 0, 0, 0, count);
     }
 
+    public ParticleGlyph(Vector velocity, int count) {
+        this(velocity, 0, 0, 0, count);
+    }
+
     public ParticleGlyph(int count) {
-        this(null, 0, 0, 0, count);
+        this((Location) null, 0, 0, 0, count);
     }
 
     public ParticleGlyph(Location toGo) {
         this(toGo, 0, 0, 0, 1);
     }
 
+    public ParticleGlyph(Vector velocity) {
+        this(velocity, 0, 0, 0, 1);
+    }
+
     public ParticleGlyph() {
-        this(null, 0, 0, 0, 1);
+        this((Location) null, 0, 0, 0, 1);
     }
 
     @Override
     public void display(Location location, Location toGo) {
         Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
         this.toGo = toGo;
 
         display(location);
 
         this.toGo = old;
+        this.velocity = oldVelocity;
     }
 
     @Override
     public void displayForPlayer(Location location, Location toGo, Player player) {
         Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
         this.toGo = toGo;
 
-        displayForPlayer(location, player);
+        displayForPlayers(location);
 
         this.toGo = old;
+        this.velocity = oldVelocity;
     }
 
     @Override
     public void displayForPlayers(Location location, Location toGo, Player... players) {
         Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
         this.toGo = toGo;
 
         displayForPlayers(location, players);
 
         this.toGo = old;
+        this.velocity = oldVelocity;
+    }
+
+    @Override
+    public void display(Location location, Vector velocity) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        display(location);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
+    }
+
+    @Override
+    public void displayForPlayer(Location location, Vector velocity, Player player) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        displayForPlayer(location, player);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
+    }
+
+    @Override
+    public void displayForPlayers(Location location, Vector velocity, Player... players) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        displayForPlayers(location, players);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
     }
 
     @Override
@@ -81,8 +149,20 @@ public class ParticleGlyph extends Particle implements TravellingParticle {
         this.toGo = location;
     }
 
-    @Override@Nullable
+    @Override
+    public void setVelocity(@Nullable Vector velocity) {
+        this.velocity = velocity;
+    }
+
+    @Nullable
+    @Override
     public Location getLocationToGo() {
         return toGo;
+    }
+
+    @Nullable
+    @Override
+    public Vector getVelocity() {
+        return velocity;
     }
 }
--- src/main/java/hm/zelha/particlesfx/particles/ParticlePortal.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticlePortal.java
@@ -1,79 +1,147 @@
 package hm.zelha.particlesfx.particles;
 
 import hm.zelha.particlesfx.particles.parents.Particle;
-import hm.zelha.particlesfx.particles.parents.TravellingParticle;
+import hm.zelha.particlesfx.particles.parents.InverseTravellingParticle;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 import javax.annotation.Nullable;
 
-public class ParticlePortal extends Particle implements TravellingParticle {
+public class ParticlePortal extends Particle implements InverseTravellingParticle {
 
     private Location toGo;
+    private Vector velocity;
 
     public ParticlePortal(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
-        super(Effect.PORTAL, offsetX, offsetY, offsetZ, 0, count, 0);
+        super(Effect.FLYING_GLYPH, offsetX, offsetY, offsetZ, 0, count, 0);
 
         this.toGo = toGo;
+    }
+
+    public ParticlePortal(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+        super(Effect.FLYING_GLYPH, offsetX, offsetY, offsetZ, 0, count, 0);
+
+        this.velocity = velocity;
     }
 
     public ParticlePortal(Location toGo, double offsetX, double offsetY, double offsetZ) {
         this(toGo, offsetX, offsetY, offsetZ, 1);
     }
 
+    public ParticlePortal(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+        this(velocity, offsetX, offsetY, offsetZ, 1);
+    }
+
     public ParticlePortal(double offsetX, double offsetY, double offsetZ, int count) {
-        this(null, offsetX, offsetY, offsetZ, count);
+        this((Location) null, offsetX, offsetY, offsetZ, count);
     }
 
     public ParticlePortal(double offsetX, double offsetY, double offsetZ) {
-        this(null, offsetX, offsetY, offsetZ, 1);
+        this((Location) null, offsetX, offsetY, offsetZ, 1);
     }
 
     public ParticlePortal(Location toGo, int count) {
         this(toGo, 0, 0, 0, count);
     }
 
+    public ParticlePortal(Vector velocity, int count) {
+        this(velocity, 0, 0, 0, count);
+    }
+
     public ParticlePortal(int count) {
-        this(null, 0, 0, 0, count);
+        this((Location) null, 0, 0, 0, count);
     }
 
     public ParticlePortal(Location toGo) {
         this(toGo, 0, 0, 0, 1);
     }
 
+    public ParticlePortal(Vector velocity) {
+        this(velocity, 0, 0, 0, 1);
+    }
+
     public ParticlePortal() {
-        this(null, 0, 0, 0, 1);
+        this((Location) null, 0, 0, 0, 1);
     }
 
     @Override
     public void display(Location location, Location toGo) {
         Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
         this.toGo = toGo;
 
         display(location);
 
         this.toGo = old;
+        this.velocity = oldVelocity;
     }
 
     @Override
     public void displayForPlayer(Location location, Location toGo, Player player) {
         Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
         this.toGo = toGo;
 
-        displayForPlayer(location, player);
+        displayForPlayers(location);
 
         this.toGo = old;
+        this.velocity = oldVelocity;
     }
 
     @Override
     public void displayForPlayers(Location location, Location toGo, Player... players) {
         Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
         this.toGo = toGo;
 
         displayForPlayers(location, players);
 
         this.toGo = old;
+        this.velocity = oldVelocity;
+    }
+
+    @Override
+    public void display(Location location, Vector velocity) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        display(location);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
+    }
+
+    @Override
+    public void displayForPlayer(Location location, Vector velocity, Player player) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        displayForPlayer(location, player);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
+    }
+
+    @Override
+    public void displayForPlayers(Location location, Vector velocity, Player... players) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        displayForPlayers(location, players);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
     }
 
     @Override
@@ -81,8 +149,20 @@ public class ParticlePortal extends Particle implements TravellingParticle {
         this.toGo = location;
     }
 
-    @Override@Nullable
+    @Override
+    public void setVelocity(@Nullable Vector velocity) {
+        this.velocity = velocity;
+    }
+
+    @Nullable
+    @Override
     public Location getLocationToGo() {
         return toGo;
+    }
+
+    @Nullable
+    @Override
+    public Vector getVelocity() {
+        return velocity;
     }
 }
