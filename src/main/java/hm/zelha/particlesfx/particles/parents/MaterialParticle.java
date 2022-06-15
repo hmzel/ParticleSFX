4cfb2858ba3fe3439073684a49150fb91eb1164c
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-15 03:23:38 -0500

    added parent interface MaterialParticle

---
+++ src/main/java/hm/zelha/particlesfx/particles/parents/MaterialParticle.java
@@ -0,0 +1,10 @@
+package hm.zelha.particlesfx.particles.parents;
+
+import org.bukkit.material.MaterialData;
+
+public interface MaterialParticle {
+
+    public void setMaterialData(MaterialData data);
+
+    public MaterialData getMaterialData();
+}
