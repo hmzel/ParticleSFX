a3006e5d44e617c558c865c42b99a1e223f7596f
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-15 02:55:22 -0500

    removed constructors with speed in ParticleInstantSpell because speed is (mostly?) unused for that particle

--- src/main/java/hm/zelha/particlesfx/particles/ParticleInstantSpell.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleInstantSpell.java
@@ -4,35 +4,19 @@ import hm.zelha.particlesfx.particles.parents.Particle;
 import org.bukkit.Effect;
 
 public class ParticleInstantSpell extends Particle {
-    public ParticleInstantSpell(double offsetX, double offsetY, double offsetZ, double speed, int count) {
-        super(Effect.INSTANT_SPELL, offsetX, offsetY, offsetZ, speed, count, 0);
-    }
-
-    public ParticleInstantSpell(double offsetX, double offsetY, double offsetZ, double speed) {
-        this(offsetX, offsetY, offsetZ, speed, 1);
-    }
-
     public ParticleInstantSpell(double offsetX, double offsetY, double offsetZ, int count) {
-        this(offsetX, offsetY, offsetZ, 0, count);
+        super(Effect.INSTANT_SPELL, offsetX, offsetY, offsetZ, 1, count, 0);
     }
 
     public ParticleInstantSpell(double offsetX, double offsetY, double offsetZ) {
-        this(offsetX, offsetY, offsetZ, 0, 1);
-    }
-
-    public ParticleInstantSpell(double speed, int count) {
-        this(0, 0, 0, speed, count);
-    }
-
-    public ParticleInstantSpell(double speed) {
-        this(0, 0, 0, speed, 1);
+        this(offsetX, offsetY, offsetZ, 1);
     }
 
     public ParticleInstantSpell(int count) {
-        this(0, 0, 0, 0, count);
+        this(0, 0, 0, count);
     }
 
     public ParticleInstantSpell() {
-        this(0, 0, 0, 0, 1);
+        this(0, 0, 0, 1);
     }
 }
