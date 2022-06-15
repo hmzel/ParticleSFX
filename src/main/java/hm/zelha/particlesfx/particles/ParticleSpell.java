51bfd1c038f7661cca50f37bca6511cd308eddf1
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-15 02:55:47 -0500

    renamed ParticleInstantSpell to ParticleSpell

--- src/main/java/hm/zelha/particlesfx/particles/ParticleSpell.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleSpell.java
@@ -3,20 +3,20 @@ package hm.zelha.particlesfx.particles;
 import hm.zelha.particlesfx.particles.parents.Particle;
 import org.bukkit.Effect;
 
-public class ParticleInstantSpell extends Particle {
-    public ParticleInstantSpell(double offsetX, double offsetY, double offsetZ, int count) {
+public class ParticleSpell extends Particle {
+    public ParticleSpell(double offsetX, double offsetY, double offsetZ, int count) {
         super(Effect.INSTANT_SPELL, offsetX, offsetY, offsetZ, 1, count, 0);
     }
 
-    public ParticleInstantSpell(double offsetX, double offsetY, double offsetZ) {
+    public ParticleSpell(double offsetX, double offsetY, double offsetZ) {
         this(offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleInstantSpell(int count) {
+    public ParticleSpell(int count) {
         this(0, 0, 0, count);
     }
 
-    public ParticleInstantSpell() {
+    public ParticleSpell() {
         this(0, 0, 0, 1);
     }
 }
