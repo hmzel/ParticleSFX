489cc37bf8d811bd851ff22de85dce5dd3a08b64
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-15 02:48:14 -0500

    removed constructors with speed in ParticleWitchMagic because speed is (mostly?) unused for that particle

--- src/main/java/hm/zelha/particlesfx/particles/ParticleWitchMagic.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleWitchMagic.java
@@ -4,35 +4,19 @@ import hm.zelha.particlesfx.particles.parents.Particle;
 import org.bukkit.Effect;
 
 public class ParticleWitchMagic extends Particle {
-    public ParticleWitchMagic(double offsetX, double offsetY, double offsetZ, double speed, int count) {
-        super(Effect.WITCH_MAGIC, offsetX, offsetY, offsetZ, speed, count, 0);
-    }
-
-    public ParticleWitchMagic(double offsetX, double offsetY, double offsetZ, double speed) {
-        this(offsetX, offsetY, offsetZ, speed, 1);
-    }
-
     public ParticleWitchMagic(double offsetX, double offsetY, double offsetZ, int count) {
-        this(offsetX, offsetY, offsetZ, 0, count);
+        super(Effect.WITCH_MAGIC, offsetX, offsetY, offsetZ, 1, count, 0);
     }
 
     public ParticleWitchMagic(double offsetX, double offsetY, double offsetZ) {
-        this(offsetX, offsetY, offsetZ, 0, 1);
-    }
-
-    public ParticleWitchMagic(double speed, int count) {
-        this(0, 0, 0, speed, count);
-    }
-
-    public ParticleWitchMagic(double speed) {
-        this(0, 0, 0, speed, 1);
+        this(offsetX, offsetY, offsetZ, 1);
     }
 
     public ParticleWitchMagic(int count) {
-        this(0, 0, 0, 0, count);
+        this(0, 0, 0, count);
     }
 
     public ParticleWitchMagic() {
-        this(0, 0, 0, 0, 1);
+        this(0, 0, 0, 1);
     }
 }
