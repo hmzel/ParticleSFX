2599a14cd5a8f47e3b97c4735ad8ce6053382ca9
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-15 19:19:21 -0500

    implemented new TravellingParticle methods into ParticleCrit

--- src/main/java/hm/zelha/particlesfx/particles/ParticleCrit.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleCrit.java
@@ -1,24 +1,37 @@
 package hm.zelha.particlesfx.particles;
 
 import hm.zelha.particlesfx.particles.parents.Particle;
-import hm.zelha.particlesfx.particles.parents.VelocityParticle;
+import hm.zelha.particlesfx.particles.parents.TravellingParticle;
 import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
 import javax.annotation.Nullable;
 
-public class ParticleCrit extends Particle implements VelocityParticle {
+public class ParticleCrit extends Particle implements TravellingParticle {
 
+    private Location toGo = null;
     private Vector velocity = null;
 
-    public ParticleCrit(Vector velocity, double offsetX, double offsetY, double offsetZ, double speed, int count) {
+    public ParticleCrit(Location toGo, double offsetX, double offsetY, double offsetZ, double speed, int count) {
         super(Effect.CRIT, offsetX, offsetY, offsetZ, speed, count, 0);
+
+        this.toGo = toGo;
+    }
+
+    public ParticleCrit(Vector velocity, double offsetX, double offsetY, double offsetZ, double speed, int count) {
+        super(Effect.CLOUD, offsetX, offsetY, offsetZ, speed, count, 0);
 
         if (velocity != null) this.velocity = velocity.multiply(0.75);
     }
 
     public ParticleCrit(double offsetX, double offsetY, double offsetZ, double speed, int count) {
-        this(null, offsetX, offsetY, offsetZ, speed, count);
+        this((Location) null, offsetX, offsetY, offsetZ, speed, count);
+    }
+
+    public ParticleCrit(Location toGo, double offsetX, double offsetY, double offsetZ) {
+        this(toGo, offsetX, offsetY, offsetZ, 1, 1);
     }
 
     public ParticleCrit(Vector velocity, double offsetX, double offsetY, double offsetZ) {
@@ -26,15 +39,19 @@ public class ParticleCrit extends Particle implements VelocityParticle {
     }
 
     public ParticleCrit(double offsetX, double offsetY, double offsetZ, double speed) {
-        this(null, offsetX, offsetY, offsetZ, speed, 1);
+        this((Location) null, offsetX, offsetY, offsetZ, speed, 1);
     }
 
     public ParticleCrit(double offsetX, double offsetY, double offsetZ, int count) {
-        this(null, offsetX, offsetY, offsetZ, 0, count);
+        this((Location) null, offsetX, offsetY, offsetZ, 0, count);
     }
 
     public ParticleCrit(double offsetX, double offsetY, double offsetZ) {
-        this(null, offsetX, offsetY, offsetZ, 0, 1);
+        this((Location) null, offsetX, offsetY, offsetZ, 0, 1);
+    }
+
+    public ParticleCrit(Location toGo, double speed, int count) {
+        this(toGo, 0, 0, 0, speed, count);
     }
 
     public ParticleCrit(Vector velocity, double speed, int count) {
@@ -42,11 +59,15 @@ public class ParticleCrit extends Particle implements VelocityParticle {
     }
 
     public ParticleCrit(double speed) {
-        this(null, 0, 0, 0, speed, 1);
+        this((Location) null, 0, 0, 0, speed, 1);
     }
 
     public ParticleCrit(int count) {
-        this(null, 0, 0, 0, 0, count);
+        this((Location) null, 0, 0, 0, 0, count);
+    }
+
+    public ParticleCrit(Location toGo) {
+        this(toGo, 0, 0, 0, 1, 1);
     }
 
     public ParticleCrit(Vector velocity) {
@@ -54,7 +75,90 @@ public class ParticleCrit extends Particle implements VelocityParticle {
     }
 
     public ParticleCrit() {
-        this(null, 0, 0, 0, 0, 1);
+        this((Location) null, 0, 0, 0, 0, 1);
+    }
+
+    @Override
+    public void display(Location location, Location toGo) {
+        Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
+        this.toGo = toGo;
+
+        display(location);
+
+        this.toGo = old;
+        this.velocity = oldVelocity;
+    }
+
+    @Override
+    public void displayForPlayer(Location location, Location toGo, Player player) {
+        Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
+        this.toGo = toGo;
+
+        displayForPlayers(location);
+
+        this.toGo = old;
+        this.velocity = oldVelocity;
+    }
+
+    @Override
+    public void displayForPlayers(Location location, Location toGo, Player... players) {
+        Location old = this.toGo;
+        Vector oldVelocity = velocity;
+        this.velocity = null;
+        this.toGo = toGo;
+
+        displayForPlayers(location, players);
+
+        this.toGo = old;
+        this.velocity = oldVelocity;
+    }
+
+    @Override
+    public void display(Location location, Vector velocity) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        display(location);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
+    }
+
+    @Override
+    public void displayForPlayer(Location location, Vector velocity, Player player) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        displayForPlayer(location, player);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
+    }
+
+    @Override
+    public void displayForPlayers(Location location, Vector velocity, Player... players) {
+        Vector old = this.velocity;
+        Location oldToGo = toGo;
+        this.toGo = null;
+        this.velocity = velocity;
+
+        displayForPlayers(location, players);
+
+        this.toGo = oldToGo;
+        this.velocity = old;
+    }
+
+    @Override
+    public void setLocationToGo(@Nullable Location location) {
+        this.toGo = location;
     }
 
     @Override
@@ -67,8 +171,19 @@ public class ParticleCrit extends Particle implements VelocityParticle {
         this.velocity = new Vector(x, y, z).multiply(0.75);
     }
 
+    @Nullable
+    @Override
+    public Location getLocationToGo() {
+        return toGo;
+    }
+
     @Override@Nullable
     public Vector getVelocity() {
         return velocity;
+    }
+
+    @Override
+    public double getVelocityControl() {
+        return 0.75;
     }
 }
