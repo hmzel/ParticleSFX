f1aee28571c54228fe5114aad0ae4d3075f2b5f1
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-19 21:10:56 -0500

    implemented particles per display into ParticleLineCompound

--- src/main/java/hm/zelha/particlesfx/shapers/ParticleLineCompound.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleLineCompound.java
@@ -14,8 +14,8 @@ public class ParticleLineCompound extends ParticleShaper {
 
     private final List<Location> locations = new ArrayList<>();
 
-    public ParticleLineCompound(Particle particle, double frequency, Location... locations) {
-        super(particle, 0, 0, 0, frequency);
+    public ParticleLineCompound(Particle particle, double frequency, int particlesPerDisplay, Location... locations) {
+        super(particle, 0, 0, 0, frequency, particlesPerDisplay);
 
         Validate.isTrue(locations.length >= 2, "Compound line must have 2 or more locations!");
 
@@ -33,29 +33,64 @@ public class ParticleLineCompound extends ParticleShaper {
         rot2.addOrigins(locations);
     }
 
+    public ParticleLineCompound(Particle particle, int particlesPerDisplay, Location... locations) {
+        this(particle, 50, particlesPerDisplay, locations);
+    }
+
+    public ParticleLineCompound(Particle particle, double frequency, Location... locations) {
+        this(particle, frequency, 0, locations);
+    }
+
     public ParticleLineCompound(Particle particle, Location... locations) {
-        this(particle, 50, locations);
+        this(particle, 50, 0, locations);
     }
 
     @Override
     public void display() {
+        int current = 0;
+        int estimatedOverallCount = 0;
+        boolean hasRan = false;
+        boolean trackCount = particlesPerDisplay != 0;
+
+        main:
         for (int i = 0; i < locations.size() - 1; i++) {
             Location start = locations.get(i);
             Location end = locations.get(i + 1);
             double distance = start.distance(end);
             double control = (distance / frequency) * locations.size();
 
+            if (trackCount && overallCount >= estimatedOverallCount + (distance / control)) {
+                estimatedOverallCount += distance / control;
+                current = overallCount - estimatedOverallCount;
+                continue;
+            }
+
             locationHelper.zero().add(start);
             LVMath.subtractToVector(vectorHelper, end, start).normalize().multiply(control);
 
-            for (double length = 0; length < distance; length += control, locationHelper.add(vectorHelper)) {
+            if (trackCount) {
+                locationHelper.add(vectorHelper.getX() * current, vectorHelper.getY() * current, vectorHelper.getZ() * current);
+            }
+
+            for (double length = control * current; length <= distance; length += control, locationHelper.add(vectorHelper)) {
                 getCurrentParticle().display(locationHelper);
 
-                currentCount++;
+                overallCount++;
+
+                if (trackCount) {
+                    currentCount++;
+                    hasRan = true;
+
+                    if (currentCount >= particlesPerDisplay) {
+                        currentCount = 0;
+                        break main;
+                    }
+                }
             }
         }
 
-        currentCount = 0;
+        if (!trackCount) overallCount = 0;
+        if (!hasRan && trackCount) overallCount = 0;
     }
 
     @Override
