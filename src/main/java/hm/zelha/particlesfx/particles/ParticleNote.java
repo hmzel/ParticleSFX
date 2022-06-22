2f59cb1dce1fe0c68fd738d07232a65ef7d97cf8
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-22 18:47:11 -0500

    merged NoteParticle and ParticleNote

--- src/main/java/hm/zelha/particlesfx/particles/ParticleNote.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleNote.java
@@ -1,6 +1,5 @@
 package hm.zelha.particlesfx.particles;
 
-import hm.zelha.particlesfx.particles.parents.NoteParticle;
 import hm.zelha.particlesfx.particles.parents.Particle;
 import org.apache.commons.lang3.Validate;
 import org.bukkit.Effect;
@@ -16,7 +15,7 @@ import org.bukkit.Effect;
  * <p></p>
  * (speed is unused regardless)
  */
-public class ParticleNote extends Particle implements NoteParticle {
+public class ParticleNote extends Particle{
 
     private NoteColor color;
 
@@ -40,15 +39,55 @@ public class ParticleNote extends Particle implements NoteParticle {
         this(NoteColor.RANDOM, 1);
     }
 
-    @Override
     public void setNoteColor(NoteColor color) {
         Validate.notNull(color, "Color cannot be null!");
 
         this.color = color;
     }
 
-    @Override
     public NoteColor getNoteColor() {
         return color;
+    }
+
+    /**
+     * it might look weird if you use NoteColor.RANDOM with other notecolors because all notecolors besides NoteColor.RANDOM
+     * are locked to 0.5, 0.5, 0.5 of the block theyre spawned at
+     */
+    enum NoteColor {
+        CYAN(0),
+        PURPLE(4),
+        PURPLE_TWO(16),
+        RED(6),
+        YELLOW(7),
+        GOLD(8),
+        BROWN(9),
+        LIGHT_BLUE(12),
+        BLUE(14),
+        GREENISH_YELLOW(21),
+        GREEN(20),
+        DESATURATED_CYAN(1),
+        DESATURATED_PURPLE(3),
+        DESATURATED_PURPLE_TWO(17),
+        DESATURATED_LIGHT_BLUE(11),
+        DESATURATED_GREEN(19),
+        SATURATED_CYAN(23),
+        SATURATED_PURPLE(5),
+        SATURATED_PURPLE_TWO(15),
+        SATURATED_LIGHT_BLUE(13),
+        SATURATED_GREEN(22),
+        GRAY(10),
+        GRAY_TWO(2),
+        GRAY_THREE(18),
+        RANDOM(-1);
+
+        private final int value;
+
+        NoteColor(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
     }
 }
