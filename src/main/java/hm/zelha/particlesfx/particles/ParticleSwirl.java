cba01cdb3c0860c3c95119eb3632fdf1af8680e9
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-22 19:53:55 -0500

    made ParticleSwirl follow the new system

--- src/main/java/hm/zelha/particlesfx/particles/ParticleSwirl.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleSwirl.java
@@ -1,25 +1,14 @@
 package hm.zelha.particlesfx.particles;
 
 import hm.zelha.particlesfx.particles.parents.ColorableParticle;
-import hm.zelha.particlesfx.particles.parents.Particle;
-import org.apache.commons.lang3.Validate;
 import org.bukkit.Color;
 import org.bukkit.Effect;
 
 import javax.annotation.Nullable;
 
-public class ParticleSwirl extends Particle implements ColorableParticle {
-
-    private Color color;
-    private int brightness;
-
+public class ParticleSwirl extends ColorableParticle {
     public ParticleSwirl(@Nullable Color color, int brightness, double offsetX, double offsetY, double offsetZ, int count) {
-        super(Effect.POTION_SWIRL, offsetX, offsetY, offsetZ, 1, count, 0);
-
-        Validate.isTrue(brightness >= 0 && brightness <= 100, "Brightness must be between 0 and 100!");
-
-        this.color = color;
-        this.brightness = brightness;
+        super(Effect.POTION_SWIRL, color, brightness, offsetX, offsetY, offsetZ, count);
     }
 
     public ParticleSwirl(double offsetX, double offsetY, double offsetZ, int count) {
@@ -48,32 +37,5 @@ public class ParticleSwirl extends Particle implements ColorableParticle {
 
     public ParticleSwirl() {
         this(null, 100, 0, 0, 0, 1);
-    }
-
-    @Override
-    public void setColor(@Nullable Color color) {
-        this.color = color;
-    }
-
-    @Override
-    public void setColor(int red, int green, int blue) {
-        this.color = Color.fromRGB(red, green, blue);
-    }
-
-    @Override
-    public void setBrightness(int brightness) {
-        Validate.isTrue(brightness >= 0 && brightness <= 100, "Brightness must be between 0 and 100!");
-
-        this.brightness = brightness;
-    }
-
-    @Override@Nullable
-    public Color getColor() {
-        return color;
-    }
-
-    @Override
-    public int getBrightness() {
-        return brightness;
     }
 }
