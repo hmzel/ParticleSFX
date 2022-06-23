9d2a868f30611e57fde723949f5caf0df1779cbc
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-22 19:31:43 -0500

    made ParticleCloud follow the new system

--- src/main/java/hm/zelha/particlesfx/particles/ParticleCloud.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleCloud.java
@@ -1,189 +1,48 @@
 package hm.zelha.particlesfx.particles;
 
-import hm.zelha.particlesfx.particles.parents.Particle;
 import hm.zelha.particlesfx.particles.parents.TravellingParticle;
 import org.bukkit.Effect;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-import javax.annotation.Nullable;
-
-public class ParticleCloud extends Particle implements TravellingParticle {
-
-    private Location toGo = null;
-    private Vector velocity = null;
-
-    public ParticleCloud(Location toGo, double offsetX, double offsetY, double offsetZ, double speed, int count) {
-        super(Effect.CLOUD, offsetX, offsetY, offsetZ, speed, count, 0);
-
-        this.toGo = toGo;
+public class ParticleCloud extends TravellingParticle {
+    public ParticleCloud(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
+        super(Effect.CLOUD, 0.05, null, toGo, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleCloud(Vector velocity, double offsetX, double offsetY, double offsetZ, double speed, int count) {
-        super(Effect.CLOUD, offsetX, offsetY, offsetZ, speed, count, 0);
-
-        if (velocity != null) this.velocity = velocity.multiply(0.05);
-    }
-
-    public ParticleCloud(double offsetX, double offsetY, double offsetZ, double speed, int count) {
-        this((Location) null, offsetX, offsetY, offsetZ, speed, count);
+    public ParticleCloud(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+        super(Effect.CLOUD, 0.05, velocity, null, offsetX, offsetY, offsetZ, count);
     }
 
     public ParticleCloud(Location toGo, double offsetX, double offsetY, double offsetZ) {
-        this(toGo, offsetX, offsetY, offsetZ, 1, 1);
+        this(toGo, offsetX, offsetY, offsetZ, 1);
     }
 
     public ParticleCloud(Vector velocity, double offsetX, double offsetY, double offsetZ) {
-        this(velocity, offsetX, offsetY, offsetZ, 1, 1);
-    }
-
-    public ParticleCloud(double offsetX, double offsetY, double offsetZ, double speed) {
-        this((Location) null, offsetX, offsetY, offsetZ, speed, 1);
+        this(velocity, offsetX, offsetY, offsetZ, 1);
     }
 
     public ParticleCloud(double offsetX, double offsetY, double offsetZ, int count) {
-        this((Location) null, offsetX, offsetY, offsetZ, 0, count);
+        this((Location) null, offsetX, offsetY, offsetZ, count);
     }
 
     public ParticleCloud(double offsetX, double offsetY, double offsetZ) {
-        this((Location) null, offsetX, offsetY, offsetZ, 0, 1);
-    }
-
-    public ParticleCloud(Location toGo, double speed, int count) {
-        this(toGo, 0, 0, 0, speed, count);
-    }
-
-    public ParticleCloud(Vector velocity, double speed, int count) {
-        this(velocity, 0, 0, 0, speed, count);
-    }
-
-    public ParticleCloud(double speed) {
-        this((Location) null, 0, 0, 0, speed, 1);
+        this((Location) null, offsetX, offsetY, offsetZ, 1);
     }
 
     public ParticleCloud(int count) {
-        this((Location) null, 0, 0, 0, 0, count);
+        this((Location) null, 0, 0, 0, count);
     }
 
     public ParticleCloud(Location toGo) {
-        this(toGo, 0, 0, 0, 1, 1);
+        this(toGo, 0, 0, 0, 1);
     }
 
     public ParticleCloud(Vector velocity) {
-        this(velocity, 0, 0, 0, 1, 1);
+        this(velocity, 0, 0, 0, 1);
     }
 
     public ParticleCloud() {
-        this((Location) null, 0, 0, 0, 0, 1);
-    }
-
-    @Override
-    public void display(Location location, Location toGo) {
-        Location old = this.toGo;
-        Vector oldVelocity = velocity;
-        this.velocity = null;
-        this.toGo = toGo;
-
-        display(location);
-
-        this.toGo = old;
-        this.velocity = oldVelocity;
-    }
-
-    @Override
-    public void displayForPlayer(Location location, Location toGo, Player player) {
-        Location old = this.toGo;
-        Vector oldVelocity = velocity;
-        this.velocity = null;
-        this.toGo = toGo;
-
-        displayForPlayers(location);
-
-        this.toGo = old;
-        this.velocity = oldVelocity;
-    }
-
-    @Override
-    public void displayForPlayers(Location location, Location toGo, Player... players) {
-        Location old = this.toGo;
-        Vector oldVelocity = velocity;
-        this.velocity = null;
-        this.toGo = toGo;
-
-        displayForPlayers(location, players);
-
-        this.toGo = old;
-        this.velocity = oldVelocity;
-    }
-
-    @Override
-    public void display(Location location, Vector velocity) {
-        Vector old = this.velocity;
-        Location oldToGo = toGo;
-        this.toGo = null;
-        this.velocity = velocity;
-
-        display(location);
-
-        this.toGo = oldToGo;
-        this.velocity = old;
-    }
-
-    @Override
-    public void displayForPlayer(Location location, Vector velocity, Player player) {
-        Vector old = this.velocity;
-        Location oldToGo = toGo;
-        this.toGo = null;
-        this.velocity = velocity;
-
-        displayForPlayer(location, player);
-
-        this.toGo = oldToGo;
-        this.velocity = old;
-    }
-
-    @Override
-    public void displayForPlayers(Location location, Vector velocity, Player... players) {
-        Vector old = this.velocity;
-        Location oldToGo = toGo;
-        this.toGo = null;
-        this.velocity = velocity;
-
-        displayForPlayers(location, players);
-
-        this.toGo = oldToGo;
-        this.velocity = old;
-    }
-
-    @Override
-    public void setLocationToGo(@Nullable Location location) {
-        this.toGo = location;
-    }
-
-    @Override
-    public void setVelocity(Vector vector) {
-        this.velocity = vector.multiply(0.05);
-    }
-
-    @Override
-    public void setVelocity(double x, double y, double z) {
-        this.velocity = new Vector(x, y, z).multiply(0.05);
-    }
-
-    @Nullable
-    @Override
-    public Location getLocationToGo() {
-        return toGo;
-    }
-
-    @Override@Nullable
-    public Vector getVelocity() {
-        return velocity;
-    }
-
-    @Override
-    public double getVelocityControl() {
-        return 0.05;
+        this((Location) null, 0, 0, 0, 1);
     }
 }
