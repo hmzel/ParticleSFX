3056254eb335ba6acd4e7ec4674033286f8b1997
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-06-22 19:48:29 -0500

    made ParticlePortal follow the new system

--- src/main/java/hm/zelha/particlesfx/particles/ParticlePortal.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticlePortal.java
@@ -1,29 +1,17 @@
 package hm.zelha.particlesfx.particles;
 
-import hm.zelha.particlesfx.particles.parents.Particle;
 import hm.zelha.particlesfx.particles.parents.InverseTravellingParticle;
 import org.bukkit.Effect;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
-import javax.annotation.Nullable;
-
-public class ParticlePortal extends Particle implements InverseTravellingParticle {
-
-    private Location toGo;
-    private Vector velocity;
-
+public class ParticlePortal extends InverseTravellingParticle {
     public ParticlePortal(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
-        super(Effect.PORTAL, offsetX, offsetY, offsetZ, 0, count, 0);
-
-        this.toGo = toGo;
+        super(Effect.PORTAL, null, toGo, offsetX, offsetY, offsetZ, count);
     }
 
     public ParticlePortal(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
-        super(Effect.PORTAL, offsetX, offsetY, offsetZ, 0, count, 0);
-
-        this.velocity = velocity;
+        super(Effect.PORTAL, velocity, null, offsetX, offsetY, offsetZ, count);
     }
 
     public ParticlePortal(Location toGo, double offsetX, double offsetY, double offsetZ) {
@@ -42,14 +30,6 @@ public class ParticlePortal extends Particle implements InverseTravellingParticl
         this((Location) null, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticlePortal(Location toGo, int count) {
-        this(toGo, 0, 0, 0, count);
-    }
-
-    public ParticlePortal(Vector velocity, int count) {
-        this(velocity, 0, 0, 0, count);
-    }
-
     public ParticlePortal(int count) {
         this((Location) null, 0, 0, 0, count);
     }
@@ -64,110 +44,5 @@ public class ParticlePortal extends Particle implements InverseTravellingParticl
 
     public ParticlePortal() {
         this((Location) null, 0, 0, 0, 1);
-    }
-
-    @Override
-    public void display(Location location, Location toGo) {
-        Location old = this.toGo;
-        Vector oldVelocity = velocity;
-        this.velocity = null;
-        this.toGo = toGo;
-
-        display(location);
-
-        this.toGo = old;
-        this.velocity = oldVelocity;
-    }
-
-    @Override
-    public void displayForPlayer(Location location, Location toGo, Player player) {
-        Location old = this.toGo;
-        Vector oldVelocity = velocity;
-        this.velocity = null;
-        this.toGo = toGo;
-
-        displayForPlayers(location);
-
-        this.toGo = old;
-        this.velocity = oldVelocity;
-    }
-
-    @Override
-    public void displayForPlayers(Location location, Location toGo, Player... players) {
-        Location old = this.toGo;
-        Vector oldVelocity = velocity;
-        this.velocity = null;
-        this.toGo = toGo;
-
-        displayForPlayers(location, players);
-
-        this.toGo = old;
-        this.velocity = oldVelocity;
-    }
-
-    @Override
-    public void display(Location location, Vector velocity) {
-        Vector old = this.velocity;
-        Location oldToGo = toGo;
-        this.toGo = null;
-        this.velocity = velocity;
-
-        display(location);
-
-        this.toGo = oldToGo;
-        this.velocity = old;
-    }
-
-    @Override
-    public void displayForPlayer(Location location, Vector velocity, Player player) {
-        Vector old = this.velocity;
-        Location oldToGo = toGo;
-        this.toGo = null;
-        this.velocity = velocity;
-
-        displayForPlayer(location, player);
-
-        this.toGo = oldToGo;
-        this.velocity = old;
-    }
-
-    @Override
-    public void displayForPlayers(Location location, Vector velocity, Player... players) {
-        Vector old = this.velocity;
-        Location oldToGo = toGo;
-        this.toGo = null;
-        this.velocity = velocity;
-
-        displayForPlayers(location, players);
-
-        this.toGo = oldToGo;
-        this.velocity = old;
-    }
-
-    @Override
-    public void setLocationToGo(@Nullable Location location) {
-        this.toGo = location;
-    }
-
-    @Override
-    public void setVelocity(@Nullable Vector velocity) {
-        this.velocity = velocity;
-    }
-
-    @Override
-    public void setVelocity(double x, double y, double z) {
-        this.velocity = new Vector(x, y, z);
-    }
-
-    @Nullable
-    @Override
-    public Location getLocationToGo() {
-        return toGo;
-    }
-
-    @Nullable
-    @Override
-    public Vector getVelocity() {
-        return velocity;
     }
 }
