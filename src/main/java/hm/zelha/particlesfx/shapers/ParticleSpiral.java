fede4c7226efbfeef18c7a6093172670107aa549
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-08-20 09:32:27 -0500

    made it so negative spin is allowed

--- src/main/java/hm/zelha/particlesfx/shapers/ParticleSpiral.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleSpiral.java
@@ -19,13 +19,14 @@ public class ParticleSpiral extends ParticleShaper {
     private final List<CircleInfo> circles = new ArrayList<>();
     private final List<Location> locations = new ArrayList<>();
     private final Vector vectorHelper2 = new Vector(0, 0, 0);
-    private CircleInfo circleHelper;
+    private final CircleInfo circleHelper;
     private double spin;
     private int count;
 
     public ParticleSpiral(Particle particle, double spin, int count, double frequency, int particlesPerDisplay, CircleInfo... circles) {
         super(particle, 0, 0, 0, frequency, particlesPerDisplay);
 
+        Validate.isTrue(count != 0, "Count cant be 0 or less!");
         Validate.isTrue(circles != null && circles.length >= 2, "Array must contain 2 or more CircleInfos!");
 
         World world = circles[0].getCenter().getWorld();
@@ -72,10 +73,10 @@ public class ParticleSpiral extends ParticleShaper {
 
     @Override
     public void display() {
-        double increase = (((Math.PI * 2) * spin) / frequency) * count;
         //used to avoid potential cases of dividing by zero without adding a bunch of if statements
         //ex: ((distance / frequency) * count) is the same as (distance * control)
         double control = (frequency / (frequency * frequency)) * count;
+        double increase = ((Math.PI * 2) * spin) * control;
 
         for (int c = 0; c < count; c++)
         for (int i = 0; i < circles.size() - 1; i++) {
@@ -96,7 +97,7 @@ public class ParticleSpiral extends ParticleShaper {
             LVMath.subtractToVector(vectorHelper, circle2.getCenter(), circle1.getCenter()).normalize().multiply(circle1.getCenter().distance(circle2.getCenter()) * control);
             circleHelper.inherit(circle1);
 
-            for (double radian = start; radian <= end; radian += increase) {
+            for (double radian = start; ((spin > 0) ? radian <= end : radian >= end); radian += increase) {
                 //setting vectorHelper2 to where the current particle should be in correlation to the current circle's center (locationHelper)
                 rot.apply(vectorHelper2.setX(circleHelper.getXRadius() * Math.cos(radian)).setY(0).setZ(circleHelper.getZRadius() * Math.sin(radian)));
                 getCurrentParticle().display(locationHelper.add(vectorHelper2));
