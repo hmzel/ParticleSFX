fc2fd2ad25697b0cea50e03b3f67bcb2cfa01e16
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-10-01 22:06:42 -0500

    added ParticleBubble class

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleBubble.java
@@ -0,0 +1,52 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.TravellingParticle;
+import net.minecraft.server.v1_8_R3.EnumParticle;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+/**
+ * EPILEPSY WARNING <p>
+ * this particle almost instantly disappears when displayed out of water, which can be displeasing to the eyes
+ */
+public class ParticleBubble extends TravellingParticle {
+    public ParticleBubble(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.WATER_BUBBLE, 0.755, null, toGo, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleBubble(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.WATER_BUBBLE, 0.755, velocity, null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleBubble(Location toGo, double offsetX, double offsetY, double offsetZ) {
+        this(toGo, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleBubble(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+        this(velocity, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleBubble(double offsetX, double offsetY, double offsetZ, int count) {
+        this((Location) null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleBubble(double offsetX, double offsetY, double offsetZ) {
+        this((Location) null, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleBubble(int count) {
+        this((Location) null, 0, 0, 0, count);
+    }
+
+    public ParticleBubble(Location toGo) {
+        this(toGo, 0, 0, 0, 1);
+    }
+
+    public ParticleBubble(Vector velocity) {
+        this(velocity, 0, 0, 0, 1);
+    }
+
+    public ParticleBubble() {
+        this((Location) null, 0, 0, 0, 1);
+    }
+}
