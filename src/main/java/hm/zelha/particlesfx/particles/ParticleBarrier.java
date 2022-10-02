6d0e2dbf670ef5fee255f4f62d536b67fcfe5d59
BuildTools <96555008+hmzel@users.noreply.github.com>
2022-10-01 23:03:31 -0500

    added ParticleBarrier class

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleBarrier.java
@@ -0,0 +1,22 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_8_R3.EnumParticle;
+
+public class ParticleBarrier extends Particle {
+    public ParticleBarrier(double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.BARRIER, offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleBarrier(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleBarrier(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleBarrier() {
+        this(0, 0, 0, 1);
+    }
+}
