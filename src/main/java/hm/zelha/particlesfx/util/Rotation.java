e9c49ff8a39d8756eacb2a780a4f7932068bb46f
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-03-20 19:18:14 -0500

    added javadocs to Rotation

--- src/main/java/hm/zelha/particlesfx/util/Rotation.java
+++ src/main/java/hm/zelha/particlesfx/util/Rotation.java
@@ -38,6 +38,10 @@ public class Rotation {
         return new Rotation().inherit(this);
     }
 
+    /**
+     * @param rotation object for this rotation class to copy data from
+     * @return this object
+     */
     public Rotation inherit(Rotation rotation) {
         if (axisRotation != null) {
             axisRotation.inherit(rotation.axisRotation);
@@ -62,6 +66,10 @@ public class Rotation {
         return this;
     }
 
+    /**
+     * @param shape object for this rotation class to copy data from
+     * @return this object
+     */
     public Rotation inherit(Shape shape) {
         Axis[] axes = shape.getRotationOrder();
 
@@ -72,6 +80,10 @@ public class Rotation {
         return this;
     }
 
+    /**
+     * @param v vector for this rotation to be applied to
+     * @return the given vector
+     */
     public Vector apply(Vector v) {
         if (axisRotation != null) {
             axisRotation.apply(v);
@@ -84,6 +96,9 @@ public class Rotation {
         return v;
     }
 
+    /**
+     * @param v vector for this rotation's pitch to be applied to
+     */
     public void applyPitch(Vector v) {
         if (pitch == 0) return;
 
@@ -107,6 +122,10 @@ public class Rotation {
         v.setY(y).setZ(z);
     }
 
+
+    /**
+     * @param v vector for this rotation's yaw to be applied to
+     */
     public void applyYaw(Vector v) {
         if (yaw == 0) return;
 
@@ -130,6 +149,9 @@ public class Rotation {
         v.setX(x).setZ(z);
     }
 
+    /**
+     * @param v vector for this rotation's roll to be applied to
+     */
     public void applyRoll(Vector v) {
         if (roll == 0) return;
 
@@ -173,10 +195,24 @@ public class Rotation {
         setRoll(this.roll + roll);
     }
 
+    /**
+     * adds the given pitch, yaw, and roll to this rotation's axis, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     *
+     * @param pitch how much to add to pitch
+     * @param yaw how much to add to yaw
+     * @param roll how much to add to roll
+     */
     public void addAxis(double pitch, double yaw, double roll) {
         axisRotation.add(pitch, yaw, roll);
     }
 
+    /**
+     * Changes the order that rotations are applied. Default is pitch, yaw, then roll.
+     *
+     * @param first first axis to be applied
+     * @param second second axis to be applied
+     * @param third third axis to be applied
+     */
     public void setRotationOrder(Axis first, Axis second, Axis third) {
         Validate.isTrue(first != second && first != third && second != third, "Can't have the same axis twice!");
 
@@ -219,22 +255,47 @@ public class Rotation {
         this.roll = roll;
     }
 
+    /**
+     * sets this rotation's axis to the given pitch, yaw, and roll, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     *
+     * @param pitch pitch to set
+     * @param yaw yaw to set
+     * @param roll roll to set
+     */
     public void setAxis(double pitch, double yaw, double roll) {
         axisRotation.set(pitch, yaw, roll);
     }
 
+    /**
+     * sets this rotation's axis to the given pitch, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     *
+     * @param pitch pitch to set
+     */
     public void setAxisPitch(double pitch) {
         axisRotation.setPitch(pitch);
     }
 
+    /**
+     * sets this rotation's axis to the given yaw, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     *
+     * @param yaw yaw to set
+     */
     public void setAxisYaw(double yaw) {
         axisRotation.setYaw(yaw);
     }
 
+    /**
+     * sets this rotation's axis to the given roll, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     *
+     * @param roll roll to set
+     */
     public void setAxisRoll(double roll) {
         axisRotation.setRoll(roll);
     }
 
+    /**
+     * @return an array of axes where [0] is the first rotation applied, [1] is the second, and [2] is the third.
+     */
     public Axis[] getRotationOrder() {
         return new Axis[] {axes[0], axes[1], axes[2]};
     }
@@ -251,14 +312,23 @@ public class Rotation {
         return roll;
     }
 
+    /**
+     * @return the pitch of this rotation's axis, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     */
     public double getAxisPitch() {
         return axisRotation.getPitch();
     }
 
+    /**
+     * @return the yaw of this rotation's axis, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     */
     public double getAxisYaw() {
         return axisRotation.getYaw();
     }
 
+    /**
+     * @return the roll of this rotation's axis, which will be applied before this rotation in {@link Rotation#apply(Vector)}
+     */
     public double getAxisRoll() {
         return axisRotation.getRoll();
     }
