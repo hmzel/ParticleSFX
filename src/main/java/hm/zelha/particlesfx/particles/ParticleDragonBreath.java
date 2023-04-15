d626262c5846a2a0e23a5ba5b77d492d0f11f730
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-15 16:40:32 -0500

    added particle class ParticleDragonBreath

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleDragonBreath.java
@@ -0,0 +1,61 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import hm.zelha.particlesfx.particles.parents.TravellingParticle;
+import net.minecraft.server.v1_9_R1.EnumParticle;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class ParticleDragonBreath extends TravellingParticle {
+    public ParticleDragonBreath(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.DRAGON_BREATH, false, 0.025, null, toGo, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleDragonBreath(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.DRAGON_BREATH, false, 0.025, velocity, null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleDragonBreath(Location toGo, double offsetX, double offsetY, double offsetZ) {
+        this(toGo, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleDragonBreath(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+        this(velocity, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleDragonBreath(double offsetX, double offsetY, double offsetZ, int count) {
+        this((Location) null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleDragonBreath(double offsetX, double offsetY, double offsetZ) {
+        this((Location) null, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleDragonBreath(int count) {
+        this((Location) null, 0, 0, 0, count);
+    }
+
+    public ParticleDragonBreath(Location toGo) {
+        this(toGo, 0, 0, 0, 1);
+    }
+
+    public ParticleDragonBreath(Vector velocity) {
+        this(velocity, 0, 0, 0, 1);
+    }
+
+    public ParticleDragonBreath() {
+        this((Location) null, 0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleDragonBreath inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleDragonBreath clone() {
+        return new ParticleDragonBreath().inherit(this);
+    }
+}
