dc8a6b192dce7cfb12467434740476c113ac6336
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-15 17:27:38 -0500

    added parent particle class SizeableParticle to handle size-related particle mechanics

--- src/main/java/hm/zelha/particlesfx/particles/ParticleExplosionLarge.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleExplosionLarge.java
@@ -1,24 +1,12 @@
 package hm.zelha.particlesfx.particles;
 
 import hm.zelha.particlesfx.particles.parents.Particle;
-import net.minecraft.server.v1_9_R1.EntityPlayer;
+import hm.zelha.particlesfx.particles.parents.SizeableParticle;
 import net.minecraft.server.v1_9_R1.EnumParticle;
-import net.minecraft.server.v1_9_R1.PacketPlayOutWorldParticles;
-import org.apache.commons.lang3.Validate;
-import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
-import org.bukkit.util.Vector;
 
-import java.util.List;
-
-public class ParticleExplosionLarge extends Particle {
-
-    private double size;
-
+public class ParticleExplosionLarge extends SizeableParticle {
     public ParticleExplosionLarge(double size, double offsetX, double offsetY, double offsetZ, int count) {
-        super(EnumParticle.EXPLOSION_LARGE, offsetX, offsetY, offsetZ, 0, count, 0);
-
-        this.size = size;
+        super(EnumParticle.EXPLOSION_LARGE, size, offsetX, offsetY, offsetZ, 0, count, 0);
     }
 
     public ParticleExplosionLarge(double size, double offsetX, double offsetY, double offsetZ) {
@@ -53,57 +41,11 @@ public class ParticleExplosionLarge extends Particle {
     public ParticleExplosionLarge inherit(Particle particle) {
         super.inherit(particle);
 
-        if (particle instanceof ParticleExplosionLarge) {
-            size = ((ParticleExplosionLarge) particle).size;
-        }
-
         return this;
     }
 
     @Override
     public ParticleExplosionLarge clone() {
         return new ParticleExplosionLarge().inherit(this);
-    }
-
-    @Override
-    protected void display(Location location, List<CraftPlayer> players) {
-        Validate.notNull(location, "Location cannot be null!");
-        Validate.notNull(location.getWorld(), "World cannot be null!");
-
-        for (int i = 0; i != count; i++) {
-            Vector addition = generateFakeOffset();
-
-            location.add(addition);
-
-            for (int i2 = 0; i2 < players.size(); i2++) {
-                EntityPlayer p = players.get(i2).getHandle();
-
-                if (p == null) continue;
-                if (!location.getWorld().getName().equals(p.world.getWorld().getName())) continue;
-
-                if (radius != 0) {
-                    double distance = Math.pow(location.getX() - p.locX, 2) + Math.pow(location.getY() - p.locY, 2) + Math.pow(location.getZ() - p.locZ, 2);
-
-                    if (distance > Math.pow(radius, 2)) continue;
-                }
-
-                p.playerConnection.sendPacket(
-                        new PacketPlayOutWorldParticles(
-                                particle, true, (float) location.getX(), (float) location.getY(), (float) location.getZ(),
-                                (float) -(size) + 2, 0, 0, (float) 1, 0
-                        )
-                );
-            }
-
-            location.subtract(addition);
-        }
-    }
-
-    public void setSize(double size) {
-        this.size = size;
-    }
-
-    public double getSize() {
-        return size;
     }
 }
