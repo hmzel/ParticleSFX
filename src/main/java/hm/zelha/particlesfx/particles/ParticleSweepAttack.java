b388408b2ccbf5e7c929684d88c4b90e563b370d
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-15 17:33:21 -0500

    added particle class ParticleSweepAttack

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleSweepAttack.java
@@ -0,0 +1,51 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import hm.zelha.particlesfx.particles.parents.SizeableParticle;
+import net.minecraft.server.v1_9_R1.EnumParticle;
+
+public class ParticleSweepAttack extends SizeableParticle {
+    public ParticleSweepAttack(double size, double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.SWEEP_ATTACK, size, offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleSweepAttack(double size, double offsetX, double offsetY, double offsetZ) {
+        this(size, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleSweepAttack(double offsetX, double offsetY, double offsetZ, int count) {
+        this(1, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleSweepAttack(double offsetX, double offsetY, double offsetZ) {
+        this(1, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleSweepAttack(double size, int count) {
+        this(size, 0, 0, 0, count);
+    }
+
+    public ParticleSweepAttack(double size) {
+        this(size, 0, 0, 0, 1);
+    }
+
+    public ParticleSweepAttack(int count) {
+        this(1, 0, 0, 0, count);
+    }
+
+    public ParticleSweepAttack() {
+        this(1, 0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleSweepAttack inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleSweepAttack clone() {
+        return new ParticleSweepAttack().inherit(this);
+    }
+}
