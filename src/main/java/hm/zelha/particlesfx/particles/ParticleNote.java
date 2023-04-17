ed0e01514d294307948e9923861edf5247276889
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-17 16:57:11 -0500

    removed all the special stuff associated with ParticleNote because PacketPlayOutBlockAction now only works if the block sent with the packet is at the BlockPosition of the particle

--- src/main/java/hm/zelha/particlesfx/particles/ParticleNote.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleNote.java
@@ -1,56 +1,28 @@
 package hm.zelha.particlesfx.particles;
 
 import hm.zelha.particlesfx.particles.parents.Particle;
-import net.minecraft.server.v1_9_R2.*;
-import org.apache.commons.lang3.Validate;
-import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import net.minecraft.server.v1_9_R2.EnumParticle;
 
-import java.util.List;
-
-/**
- * due to how the minecraft systems work, offset is only usable if the color is NoteColor.RANDOM,
- * so I didn't bother to add constructors for that <br>
- * feel free to use setOffset() methods though!
- * <br><br>
- * also, all ParticleNotes with a NoteColor other than NoteColor.RANDOM have a fixed radius that is quite small,
- * due to minecraft systems, again.
- * <br><br>
- * (speed is unused regardless)
- */
 public class ParticleNote extends Particle {
-
-    private NoteColor color;
-
-    /**@see ParticleNote*/
-    public ParticleNote(NoteColor color, int count) {
-        super(EnumParticle.NOTE, 0, 0, 0, 0, count, 0);
-
-        setNoteColor(color);
+    public ParticleNote(double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.BARRIER, offsetX, offsetY, offsetZ, 0, count, 0);
     }
 
-    /**@see ParticleNote*/
-    public ParticleNote(NoteColor color) {
-        this(color, 1);
+    public ParticleNote(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
     }
 
-    /**@see ParticleNote*/
     public ParticleNote(int count) {
-        this(NoteColor.RANDOM, count);
+        this(0, 0, 0, count);
     }
 
-    /**@see ParticleNote*/
     public ParticleNote() {
-        this(NoteColor.RANDOM, 1);
+        this(0, 0, 0, 1);
     }
 
     @Override
     public ParticleNote inherit(Particle particle) {
         super.inherit(particle);
-
-        if (particle instanceof ParticleNote) {
-            color = ((ParticleNote) particle).color;
-        }
 
         return this;
     }
@@ -58,94 +30,5 @@ public class ParticleNote extends Particle {
     @Override
     public ParticleNote clone() {
         return new ParticleNote().inherit(this);
-    }
-
-    @Override
-    protected void display(Location location, List<CraftPlayer> players) {
-        Validate.notNull(location, "Location cannot be null!");
-        Validate.notNull(location.getWorld(), "World cannot be null!");
-
-        Packet packet = null;
-
-        if (color != NoteColor.RANDOM) {
-            packet = new PacketPlayOutBlockAction(new BlockPosition(
-                    location.getBlockX(), location.getBlockY(), location.getBlockZ()
-            ), Blocks.NOTEBLOCK, 0, color.getValue());
-        }
-
-        for (int i = 0; i < (((color != NoteColor.RANDOM)) ? count : 1); i++) {
-            for (int i2 = 0; i2 < players.size(); i2++) {
-                EntityPlayer p = players.get(i2).getHandle();
-
-                if (p == null) continue;
-                if (!location.getWorld().getName().equals(p.world.getWorld().getName())) continue;
-
-                if (radius != 0) {
-                    double distance = Math.pow(location.getX() - p.locX, 2) + Math.pow(location.getY() - p.locY, 2) + Math.pow(location.getZ() - p.locZ, 2);
-
-                    if (distance > Math.pow(radius, 2)) continue;
-                }
-
-                p.playerConnection.sendPacket((packet != null) ? packet :
-                        new PacketPlayOutWorldParticles(
-                                EnumParticle.NOTE, true, (float) location.getX(), (float) location.getY(), (float) location.getZ(),
-                                (float) offsetX, (float) offsetY, (float) offsetZ, (float) 1, count
-                        )
-                );
-            }
-        }
-    }
-
-    public void setNoteColor(NoteColor color) {
-        Validate.notNull(color, "Color cannot be null!");
-
-        this.color = color;
-    }
-
-    public NoteColor getNoteColor() {
-        return color;
-    }
-
-
-    /**
-     * it might look weird if you use NoteColor.RANDOM with other notecolors because all notecolors besides NoteColor.RANDOM
-     * are locked to 0.5, 0.5, 0.5 of the block they're spawned at
-     */
-    public enum NoteColor {
-        CYAN(0),
-        PURPLE(4),
-        PURPLE_TWO(16),
-        RED(6),
-        YELLOW(7),
-        GOLD(8),
-        BROWN(9),
-        LIGHT_BLUE(12),
-        BLUE(14),
-        GREENISH_YELLOW(21),
-        GREEN(20),
-        DESATURATED_CYAN(1),
-        DESATURATED_PURPLE(3),
-        DESATURATED_PURPLE_TWO(17),
-        DESATURATED_LIGHT_BLUE(11),
-        DESATURATED_GREEN(19),
-        SATURATED_CYAN(23),
-        SATURATED_PURPLE(5),
-        SATURATED_PURPLE_TWO(15),
-        SATURATED_LIGHT_BLUE(13),
-        SATURATED_GREEN(22),
-        GRAY(10),
-        GRAY_TWO(2),
-        GRAY_THREE(18),
-        RANDOM(-1);
-
-        private final int value;
-
-        NoteColor(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
     }
 }
