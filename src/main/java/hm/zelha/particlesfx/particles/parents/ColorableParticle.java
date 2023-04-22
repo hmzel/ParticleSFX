40476b04bc554f01bf70d87daa12685f56f326f6
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-21 21:47:06 -0500

    refactored ColorableParticle
    (cherry picked from commit 4e0bb38dfe3aff25f981e7a12d6482f2ef423ee1)

--- src/main/java/hm/zelha/particlesfx/particles/parents/ColorableParticle.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/ColorableParticle.java
@@ -1,16 +1,12 @@
 package hm.zelha.particlesfx.particles.parents;
 
-import net.minecraft.server.v1_8_R3.EntityPlayer;
 import net.minecraft.server.v1_8_R3.EnumParticle;
-import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
 import org.apache.commons.lang3.Validate;
 import hm.zelha.particlesfx.util.Color;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.util.Vector;
 
 import javax.annotation.Nullable;
-import java.util.List;
 
 public abstract class ColorableParticle extends Particle {
 
@@ -41,55 +37,41 @@ public abstract class ColorableParticle extends Particle {
     public abstract ColorableParticle clone();
 
     @Override
-    protected void display(Location location, List<CraftPlayer> players) {
-        Validate.notNull(location, "Location cannot be null!");
-        Validate.notNull(location.getWorld(), "World cannot be null!");
+    protected Vector getXYZ(Location location) {
+        Vector xyz = super.getXYZ(location);
 
-        for (int i = 0; i < ((color != null) ? count : 1); i++) {
-            int trueCount = count;
-            double trueOffsetX = offsetX;
-            double trueOffsetY = offsetY;
-            double trueOffsetZ = offsetZ;
-            double trueSpeed;
-            Vector addition = null;
-
-            if (color != null) {
-                trueCount = 0;
-                trueOffsetX = color.getRed() / 255D;
-                trueOffsetY = color.getGreen() / 255D;
-                trueOffsetZ = color.getBlue() / 255D;
-                trueSpeed = brightness * 0.01;
-                addition = generateFakeOffset();
-
-                location.add(addition);
-            } else {
-                trueSpeed = 1;
-            }
-
-            for (int i2 = 0; i2 < players.size(); i2++) {
-                EntityPlayer p = players.get(i2).getHandle();
-
-                if (p == null) continue;
-                if (!location.getWorld().getName().equals(p.world.getWorld().getName())) continue;
-
-                if (radius != 0) {
-                    double distance = Math.pow(location.getX() - p.locX, 2) + Math.pow(location.getY() - p.locY, 2) + Math.pow(location.getZ() - p.locZ, 2);
-
-                    if (distance > Math.pow(radius, 2)) continue;
-                }
-
-                p.playerConnection.sendPacket(
-                        new PacketPlayOutWorldParticles(
-                                particle, true, (float) location.getX(), (float) location.getY(), (float) location.getZ(),
-                                (float) trueOffsetX, (float) trueOffsetY, (float) trueOffsetZ, (float) trueSpeed, trueCount
-                        )
-                );
-            }
-
-            if (addition != null) {
-                location.subtract(addition);
-            }
+        if (color != null) {
+            xyz.add(generateFakeOffset());
         }
+
+        return xyz;
+    }
+
+    @Override
+    protected Vector getOffsets(Location location) {
+        Vector offsets = super.getOffsets(location);
+
+        if (color != null) {
+            offsets.setX(color.getRed() / 255D);
+            offsets.setY(color.getGreen() / 255D);
+            offsets.setZ(color.getBlue() / 255D);
+        }
+
+        return offsets;
+    }
+
+    @Override
+    protected float getPacketSpeed() {
+        if (color != null) return (float) (brightness * 0.01);
+
+        return super.getPacketSpeed();
+    }
+
+    @Override
+    protected int getPacketCount() {
+        if (color != null) return 0;
+
+        return super.getCount();
     }
 
     /**
