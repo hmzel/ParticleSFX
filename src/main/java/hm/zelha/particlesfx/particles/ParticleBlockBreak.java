68810d837f03603dcacad15af470de7e0a064141
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-21 22:15:38 -0500

    made ParticleBlockBreak work with the changes to parent classes
    (cherry picked from commit 37d56b394e063570a9d5587eaa4bbb3645923c7b)

--- src/main/java/hm/zelha/particlesfx/particles/ParticleBlockBreak.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleBlockBreak.java
@@ -1,8 +1,10 @@
 package hm.zelha.particlesfx.particles;
 
-import hm.zelha.particlesfx.particles.parents.MaterialParticle;
 import hm.zelha.particlesfx.particles.parents.Particle;
 import net.minecraft.server.v1_8_R3.EnumParticle;
+import hm.zelha.particlesfx.particles.parents.TravellingParticle;
+import hm.zelha.particlesfx.particles.parents.MaterialParticle;
+import org.apache.commons.lang3.Validate;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.material.MaterialData;
@@ -11,17 +13,20 @@ import org.bukkit.util.Vector;
 /**
  * warning: the speed of this particle is inconsistent due to gravity and other factors that aren't accounted for
  */
-public class ParticleBlockBreak extends MaterialParticle {
+public class ParticleBlockBreak extends TravellingParticle implements MaterialParticle {
+
+    protected MaterialData data;
+
     /**@see ParticleBlockBreak*/
     public ParticleBlockBreak(MaterialData data, Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
-        super(EnumParticle.BLOCK_DUST, false, 0.105, data, velocity, null, offsetX, offsetY, offsetZ, count);
+        super(EnumParticle.BLOCK_DUST, false, 0.105, velocity, null, offsetX, offsetY, offsetZ, count);
 
         setMaterialData(data);
     }
 
     /**@see ParticleBlockBreak*/
     public ParticleBlockBreak(MaterialData data, Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
-        super(EnumParticle.BLOCK_DUST, false, 0.105, data, null, toGo, offsetX, offsetY, offsetZ, count);
+        super(EnumParticle.BLOCK_DUST, false, 0.105, null, toGo, offsetX, offsetY, offsetZ, count);
 
         setMaterialData(data);
     }
@@ -125,6 +130,10 @@ public class ParticleBlockBreak extends MaterialParticle {
     public ParticleBlockBreak inherit(Particle particle) {
         super.inherit(particle);
 
+        if (particle instanceof ParticleBlockBreak) {
+            data = ((ParticleBlockBreak) particle).data;
+        }
+
         return this;
     }
 
@@ -136,5 +145,16 @@ public class ParticleBlockBreak extends MaterialParticle {
     @Override
     protected int[] getPacketData() {
         return new int[] {(data.getData() << 12 | data.getItemTypeId() & 4095)};
+    }
+
+    public void setMaterialData(MaterialData data) {
+        Validate.notNull(data, "Data cannot be null!");
+        Validate.isTrue(data.getItemType().isBlock(), "Material must be a block!");
+
+        this.data = data;
+    }
+
+    public MaterialData getMaterialData() {
+        return data;
     }
 }
