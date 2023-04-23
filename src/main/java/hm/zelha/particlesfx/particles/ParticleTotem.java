95639d85ee2ebef979c61ef14996bc78c31eaed5
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-23 15:32:30 -0500

    added particle class ParticleTotem

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleTotem.java
@@ -0,0 +1,61 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import hm.zelha.particlesfx.particles.parents.TravellingParticle;
+import net.minecraft.server.v1_11_R1.EnumParticle;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class ParticleTotem extends TravellingParticle {
+    public ParticleTotem(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.TOTEM, false, 0.44, null, toGo, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleTotem(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.TOTEM, false, 0.44, velocity, null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleTotem(Location toGo, double offsetX, double offsetY, double offsetZ) {
+        this(toGo, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleTotem(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+        this(velocity, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleTotem(double offsetX, double offsetY, double offsetZ, int count) {
+        this((Location) null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleTotem(double offsetX, double offsetY, double offsetZ) {
+        this((Location) null, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleTotem(int count) {
+        this((Location) null, 0, 0, 0, count);
+    }
+
+    public ParticleTotem(Location toGo) {
+        this(toGo, 0, 0, 0, 1);
+    }
+
+    public ParticleTotem(Vector velocity) {
+        this(velocity, 0, 0, 0, 1);
+    }
+
+    public ParticleTotem() {
+        this((Location) null, 0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleTotem inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleTotem clone() {
+        return new ParticleTotem().inherit(this);
+    }
+}
