35ff2ee4e26742b94a2bd43f194b65505225fee9
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-30 09:13:32 -0500

    renamed ParticleFireworksSpark to ParticleFirework and updated it to 1.13

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleFirework.java
@@ -0,0 +1,62 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import hm.zelha.particlesfx.particles.parents.TravellingParticle;
+import net.minecraft.server.v1_13_R1.MinecraftKey;
+import net.minecraft.server.v1_13_R1.ParticleType;
+import org.bukkit.Location;
+import org.bukkit.util.Vector;
+
+public class ParticleFirework extends TravellingParticle {
+    public ParticleFirework(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
+        super((ParticleType) REGISTRY.get(new MinecraftKey("firework")), false, 0.103, null, toGo, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleFirework(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+        super((ParticleType) REGISTRY.get(new MinecraftKey("firework")), false, 0.103, velocity, null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleFirework(Location toGo, double offsetX, double offsetY, double offsetZ) {
+        this(toGo, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleFirework(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+        this(velocity, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleFirework(double offsetX, double offsetY, double offsetZ, int count) {
+        this((Location) null, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleFirework(double offsetX, double offsetY, double offsetZ) {
+        this((Location) null, offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleFirework(int count) {
+        this((Location) null, 0, 0, 0, count);
+    }
+
+    public ParticleFirework(Location toGo) {
+        this(toGo, 0, 0, 0, 1);
+    }
+
+    public ParticleFirework(Vector velocity) {
+        this(velocity, 0, 0, 0, 1);
+    }
+
+    public ParticleFirework() {
+        this((Location) null, 0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleFirework inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleFirework clone() {
+        return new ParticleFirework().inherit(this);
+    }
+}
