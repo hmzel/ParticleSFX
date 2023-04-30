af66927385aa90309fb9bd1ea6f2c4a5bd91dc22
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-04-30 18:56:08 -0500

    added some javadocs to ParticleCurrentDown

--- src/main/java/hm/zelha/particlesfx/particles/ParticleCurrentDown.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleCurrentDown.java
@@ -4,19 +4,26 @@ import hm.zelha.particlesfx.particles.parents.Particle;
 import net.minecraft.server.v1_13_R1.MinecraftKey;
 import net.minecraft.server.v1_13_R1.ParticleType;
 
+/**
+ * this particle almost instantly disappears when displayed out of water, which can be displeasing to the eyes
+ */
 public class ParticleCurrentDown extends Particle {
+    /** @see ParticleCurrentDown */
     public ParticleCurrentDown(double offsetX, double offsetY, double offsetZ, int count) {
         super((ParticleType) REGISTRY.get(new MinecraftKey("current_down")), offsetX, offsetY, offsetZ, 0, count, 0);
     }
 
+    /** @see ParticleCurrentDown */
     public ParticleCurrentDown(double offsetX, double offsetY, double offsetZ) {
         this(offsetX, offsetY, offsetZ, 1);
     }
 
+    /** @see ParticleCurrentDown */
     public ParticleCurrentDown(int count) {
         this(0, 0, 0, count);
     }
 
+    /** @see ParticleCurrentDown */
     public ParticleCurrentDown() {
         this(0, 0, 0, 1);
     }
