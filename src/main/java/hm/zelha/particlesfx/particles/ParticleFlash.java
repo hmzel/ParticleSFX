f9e835ca8f51667d9dd9c4837e8d9b59bcfa6fca
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-05 03:51:24 -0500

    added particle class ParticleFlash

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleFlash.java
@@ -0,0 +1,34 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_14_R1.Particles;
+
+public class ParticleFlash extends Particle {
+    public ParticleFlash(double offsetX, double offsetY, double offsetZ, int count) {
+        super(Particles.FLASH, offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleFlash(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleFlash(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleFlash() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleFlash inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleFlash clone() {
+        return new ParticleFlash().inherit(this);
+    }
+}
