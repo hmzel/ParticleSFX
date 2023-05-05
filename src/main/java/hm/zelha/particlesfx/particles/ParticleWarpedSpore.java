c7a03a5d44f23a11d878082b1e368f6f59c042ed
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-05 17:57:41 -0500

    added particle class ParticleWarpedSpore

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleWarpedSpore.java
@@ -0,0 +1,34 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_16_R1.Particles;
+
+public class ParticleWarpedSpore extends Particle {
+    public ParticleWarpedSpore(double offsetX, double offsetY, double offsetZ, int count) {
+        super(Particles.WARPED_SPORE, offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleWarpedSpore(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleWarpedSpore(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleWarpedSpore() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleWarpedSpore inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleWarpedSpore clone() {
+        return new ParticleWarpedSpore().inherit(this);
+    }
+}
