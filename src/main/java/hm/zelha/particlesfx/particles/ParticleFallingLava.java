40c17113b93d10be0a85d9d20759eb25fa5c7f35
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-05 03:39:04 -0500

    added particle class ParticleFallingLava

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleFallingLava.java
@@ -0,0 +1,34 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_14_R1.Particles;
+
+public class ParticleFallingLava extends Particle {
+    public ParticleFallingLava(double offsetX, double offsetY, double offsetZ, int count) {
+        super(Particles.FALLING_LAVA, offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleFallingLava(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleFallingLava(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleFallingLava() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleFallingLava inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleFallingLava clone() {
+        return new ParticleFallingLava().inherit(this);
+    }
+}
