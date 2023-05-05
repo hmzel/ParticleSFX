04489f616588a8460c54fd5a9622cb41a6f65226
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-05 03:33:26 -0500

    Renamed ParticleDripLava to ParticleDrippingLava and renamed ParticleDripWater to ParticleDrippingWater

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleDrippingLava.java
@@ -0,0 +1,33 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+
+public class ParticleDrippingLava extends Particle {
+    public ParticleDrippingLava(double offsetX, double offsetY, double offsetZ, int count) {
+        super("dripping_lava", offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleDrippingLava(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleDrippingLava(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleDrippingLava() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleDrippingLava inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleDrippingLava clone() {
+        return new ParticleDrippingLava().inherit(this);
+    }
+}
