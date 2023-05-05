9de1652588e61e6466f13bd591fd27c93a94cf63
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-05 17:55:18 -0500

    added particle class ParticleAsh

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleAsh.java
@@ -0,0 +1,34 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_16_R1.Particles;
+
+public class ParticleAsh extends Particle {
+    public ParticleAsh(double offsetX, double offsetY, double offsetZ, int count) {
+        super(Particles.ASH, offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleAsh(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleAsh(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleAsh() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleAsh inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleAsh clone() {
+        return new ParticleAsh().inherit(this);
+    }
+}
