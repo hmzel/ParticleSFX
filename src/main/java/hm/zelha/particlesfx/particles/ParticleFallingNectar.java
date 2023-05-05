7c3936eaa1debd9ca2cb893f0ce9dd00f2f4d75f
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-05 16:01:36 -0500

    added particle class ParticleFallingNectar

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleFallingNectar.java
@@ -0,0 +1,34 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_15_R1.Particles;
+
+public class ParticleFallingNectar extends Particle {
+    public ParticleFallingNectar(double offsetX, double offsetY, double offsetZ, int count) {
+        super(Particles.FALLING_NECTAR, offsetX, offsetY, offsetZ, 0, count, 0);
+    }
+
+    public ParticleFallingNectar(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleFallingNectar(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleFallingNectar() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleFallingNectar inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleFallingNectar clone() {
+        return new ParticleFallingNectar().inherit(this);
+    }
+}
