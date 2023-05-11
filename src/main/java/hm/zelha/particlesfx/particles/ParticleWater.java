4f45cfc6215f8e9fafe464953ffbd26fbba102c5
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-10 23:24:57 -0500

    removed javadoc from ParticleWater because its no longer valid
    (cherry picked from commit 06c2388e368b6219979513da612fd75a550e7ee7)

--- src/main/java/hm/zelha/particlesfx/particles/ParticleWater.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleWater.java
@@ -7,58 +7,43 @@ import net.minecraft.server.v1_13_R1.ParticleType;
 import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
-/**
- * NOTE: velocity on this particle can't be controlled! <br>
- * both velocity and location to go variables will be completely inaccurate. not my fault <br><br>
- * no matter how low you set it, it'll always go at least 14-21 blocks in the direction its set. blame mojang
- */
 public class ParticleWater extends TravellingParticle {
-    /**@see ParticleWater*/
     public ParticleWater(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
         super((ParticleType) REGISTRY.get(new MinecraftKey("fishing")), false, 0.05, null, toGo, offsetX, offsetY, offsetZ, count);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
         super((ParticleType) REGISTRY.get(new MinecraftKey("fishing")), false, 0.05, velocity, null, offsetX, offsetY, offsetZ, count);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(Location toGo, double offsetX, double offsetY, double offsetZ) {
         this(toGo, offsetX, offsetY, offsetZ, 1);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(Vector velocity, double offsetX, double offsetY, double offsetZ) {
         this(velocity, offsetX, offsetY, offsetZ, 1);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(double offsetX, double offsetY, double offsetZ, int count) {
         this((Location) null, offsetX, offsetY, offsetZ, count);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(double offsetX, double offsetY, double offsetZ) {
         this((Location) null, offsetX, offsetY, offsetZ, 1);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(int count) {
         this((Location) null, 0, 0, 0, count);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(Location toGo) {
         this(toGo, 0, 0, 0, 1);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater(Vector velocity) {
         this(velocity, 0, 0, 0, 1);
     }
 
-    /**@see ParticleWater*/
     public ParticleWater() {
         this((Location) null, 0, 0, 0, 1);
     }
