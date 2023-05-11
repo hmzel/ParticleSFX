ff743e3871aba96fa6abb411fba98907df14aa52
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-05-11 18:38:04 -0500

    fixed a bug with the latest commit causing ParticleEnderSignal to throw an error when constructed

--- src/main/java/hm/zelha/particlesfx/particles/ParticleEnderSignal.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleEnderSignal.java
@@ -17,7 +17,7 @@ import org.bukkit.Location;
 public class ParticleEnderSignal extends Particle {
     /**@see ParticleEnderSignal*/
     public ParticleEnderSignal(int count) {
-        super(null, 0, 0, 0, 0, count, 0);
+        super("", 0, 0, 0, 0, count, 0);
     }
 
     /**@see ParticleEnderSignal*/
