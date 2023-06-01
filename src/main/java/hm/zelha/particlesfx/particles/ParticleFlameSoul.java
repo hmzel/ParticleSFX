840773545d3448e345e537bcde808ab867901873
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-06-01 08:46:33 -0500

    renamed some particle classes
    (cherry picked from commit 0801c05d933e1ee28c8ebf7108c28780e379c852)

--- src/main/java/hm/zelha/particlesfx/particles/ParticleFlameSoul.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleFlameSoul.java
@@ -6,56 +6,56 @@ import net.minecraft.server.v1_16_R2.Particles;
 import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
-public class ParticleSoulFlame extends TravellingParticle {
-    public ParticleSoulFlame(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
+public class ParticleFlameSoul extends TravellingParticle {
+    public ParticleFlameSoul(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
         super(Particles.SOUL_FIRE_FLAME, false, 0.07, null, toGo, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleSoulFlame(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+    public ParticleFlameSoul(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
         super(Particles.SOUL_FIRE_FLAME, false, 0.07, velocity, null, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleSoulFlame(Location toGo, double offsetX, double offsetY, double offsetZ) {
+    public ParticleFlameSoul(Location toGo, double offsetX, double offsetY, double offsetZ) {
         this(toGo, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleSoulFlame(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+    public ParticleFlameSoul(Vector velocity, double offsetX, double offsetY, double offsetZ) {
         this(velocity, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleSoulFlame(double offsetX, double offsetY, double offsetZ, int count) {
+    public ParticleFlameSoul(double offsetX, double offsetY, double offsetZ, int count) {
         this((Location) null, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleSoulFlame(double offsetX, double offsetY, double offsetZ) {
+    public ParticleFlameSoul(double offsetX, double offsetY, double offsetZ) {
         this((Location) null, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleSoulFlame(int count) {
+    public ParticleFlameSoul(int count) {
         this((Location) null, 0, 0, 0, count);
     }
 
-    public ParticleSoulFlame(Location toGo) {
+    public ParticleFlameSoul(Location toGo) {
         this(toGo, 0, 0, 0, 1);
     }
 
-    public ParticleSoulFlame(Vector velocity) {
+    public ParticleFlameSoul(Vector velocity) {
         this(velocity, 0, 0, 0, 1);
     }
 
-    public ParticleSoulFlame() {
+    public ParticleFlameSoul() {
         this((Location) null, 0, 0, 0, 1);
     }
 
     @Override
-    public ParticleSoulFlame inherit(Particle particle) {
+    public ParticleFlameSoul inherit(Particle particle) {
         super.inherit(particle);
 
         return this;
     }
 
     @Override
-    public ParticleSoulFlame clone() {
-        return new ParticleSoulFlame().inherit(this);
+    public ParticleFlameSoul clone() {
+        return new ParticleFlameSoul().inherit(this);
     }
 }
