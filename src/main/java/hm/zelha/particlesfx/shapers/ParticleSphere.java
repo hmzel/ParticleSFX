99add3c7a7a5891e488a14da6a845b984f9cc49d
BuildTools <96555008+hmzel@users.noreply.github.com>
2023-06-06 18:30:49 -0500

    renamed ParticleSphere to ParticleSphereCSA and renamed ParticleSphereSFSA to ParticleSphere
    (cherry picked from commit 1b46bd76419bca1b55d2092e3743c751a84e5290)

--- src/main/java/hm/zelha/particlesfx/shapers/ParticleSphere.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleSphere.java
@@ -4,144 +4,98 @@ import hm.zelha.particlesfx.particles.parents.Particle;
 import hm.zelha.particlesfx.util.LocationSafe;
 import hm.zelha.particlesfx.util.ParticleShapeCompound;
 import hm.zelha.particlesfx.util.ShapeDisplayMechanic;
-import org.apache.commons.lang.Validate;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
- * This class uses surface area and circumference to position particles, as opposed to {@link ParticleSphereSFSA} which uses the
- * the <a href="https://medium.com/@vagnerseibert/distributing-points-on-a-sphere-6b593cc05b42">Sunflower Seed Arrangement</a>
+ * This class uses the <a href="https://medium.com/@vagnerseibert/distributing-points-on-a-sphere-6b593cc05b42">Sunflower Seed Arrangement</a>
+ * to form a sphere, as opposed to {@link ParticleSphereCSA} which uses surface area and circumference.
  * <br><br>
- * This class's sphere looks worse, but is much less chaotic and easier to color with secondary particles.
+ * Using the sunflower seed arrangement uses almost the same processing power, but makes the sphere look much prettier and makes it so you
+ * don't have to worry about CircleFrequency. <br>
+ * however, using this makes it much harder to color the sphere with secondary particles, because the points are drawn pretty chaotically.
+ * <br><br>
+ * TLDR: Use this class if you just want a normal sphere or ellipsoid, otherwise use {@link ParticleSphereCSA}.
  */
-public class ParticleSphere extends ParticleSphereSFSA {
+public class ParticleSphere extends ParticleCircle {
 
-    //circumference tracker
-    protected final List<Double> cirTracker = new ArrayList<>();
-    protected int circleFrequency;
-    protected double surfaceArea = 0;
-    protected boolean recalculate = true;
+    protected double yRadius;
 
-    /**
-     * Note: if circleFrequency is too high it can cause the sphere to look weird, if that's why you're looking at this then I'd suggest lowering it.
-     *
-     * @see ParticleSphere
-     */
-    public ParticleSphere(Particle particle, LocationSafe center, double xRadius, double yRadius, double zRadius, double pitch, double yaw, double roll, int circleFrequency, int particleFrequency) {
-        super(particle, center, xRadius, yRadius, zRadius, pitch, yaw, roll, particleFrequency);
+    /**@see ParticleSphere */
+    public ParticleSphere(Particle particle, LocationSafe center, double xRadius, double yRadius, double zRadius, double pitch, double yaw, double roll, int particleFrequency) {
+        super(particle, center, xRadius, zRadius, pitch, yaw, roll, particleFrequency);
 
-        setCircleFrequency(circleFrequency);
+        setYRadius(yRadius);
     }
 
-    /**
-     * Note: if circleFrequency is too high it can cause the sphere to look weird, if that's why you're looking at this then I'd suggest lowering it.
-     *
-     * @see ParticleSphere
-     */
-    public ParticleSphere(Particle particle, LocationSafe center, double radius, double pitch, double yaw, double roll, int circleFrequency, int particleFrequency) {
-        this(particle, center, radius, radius, radius, pitch, yaw, roll, circleFrequency, particleFrequency);
+    /**@see ParticleSphere */
+    public ParticleSphere(Particle particle, LocationSafe center, double radius, double pitch, double yaw, double roll, int particleFrequency) {
+        this(particle, center, radius, radius, radius, pitch, yaw, roll, particleFrequency);
     }
 
-    /**
-     * Note: if circleFrequency is too high it can cause the sphere to look weird, if that's why you're looking at this then I'd suggest lowering it.
-     *
-     * @see ParticleSphere
-     */
-    public ParticleSphere(Particle particle, LocationSafe center, double xRadius, double yRadius, double zRadius, int circleFrequency, int particleFrequency) {
-        this(particle, center, xRadius, yRadius, zRadius, 0, 0, 0, circleFrequency, particleFrequency);
-    }
-
-    /**
-     * Note: if circleFrequency is too high it can cause the sphere to look weird, if that's why you're looking at this then I'd suggest lowering it.
-     *
-     * @see ParticleSphere
-     */
-    public ParticleSphere(Particle particle, LocationSafe center, double radius, int circleFrequency, int particleFrequency) {
-        this(particle, center, radius, radius, radius, 0, 0, 0, circleFrequency, particleFrequency);
-    }
-
-    /**@see ParticleSphere*/
+    /**@see ParticleSphere */
     public ParticleSphere(Particle particle, LocationSafe center, double xRadius, double yRadius, double zRadius, int particleFrequency) {
-        this(particle, center, xRadius, yRadius, zRadius, 0, 0, 0, (int) (particleFrequency * 0.021), particleFrequency);
+        this(particle, center, xRadius, yRadius, zRadius, 0, 0, 0, particleFrequency);
     }
 
-    /**@see ParticleSphere*/
+    /**@see ParticleSphere */
     public ParticleSphere(Particle particle, LocationSafe center, double radius, int particleFrequency) {
-        this(particle, center, radius, radius, radius, 0, 0, 0, (int) (particleFrequency * 0.021), particleFrequency);
+        this(particle, center, radius, radius, radius, 0, 0, 0, particleFrequency);
     }
 
-    /**@see ParticleSphere*/
+    /**@see ParticleSphere */
     public ParticleSphere(Particle particle, LocationSafe center, double radius) {
-        this(particle, center, radius, radius, radius, 0, 0, 0, (int) (radius * 75 * 0.021), (int) (radius * 75));
+        this(particle, center, radius, radius, radius, 0, 0, 0, (int) (radius * 75));
     }
 
     @Override
     public void display() {
         boolean hasRan = false;
         boolean trackCount = particlesPerDisplay > 0;
-        int current = overallCount;
 
-        if (recalculate) {
-            recalcCircumferenceAndArea();
-        }
+        for (int i = overallCount; i < particleFrequency; i++) {
+            Particle particle = getCurrentParticle();
+            double limit = particleFrequency - (i - (i * (this.limit / 100)));
 
-        main:
-        for (int i = 0; i < circleFrequency; i++) {
-            double curveRadian = (Math.PI - (Math.PI * limit / 100)) / (circleFrequency - 1) * i;
-            int particleAmount = (int) Math.max((particleFrequency - (circleFrequency / 2D)) * (cirTracker.get(i) / surfaceArea), 1);
-
-            if (!limitInverse) {
-                curveRadian += Math.PI * limit / 100;
+            if (limitInverse) {
+                limit = i - (i * (this.limit / 100));
             }
 
-            if (current >= particleAmount) {
-                current -= particleAmount;
+            double curveRadian = Math.acos(1 - 2D * limit / particleFrequency);
+            double radian = Math.PI * (1 + Math.sqrt(5)) * i;
 
-                continue;
+            vectorHelper.setX(xRadius * Math.sin(curveRadian) * Math.cos(radian));
+            vectorHelper.setY(yRadius * Math.cos(curveRadian));
+            vectorHelper.setZ(zRadius * Math.sin(curveRadian) * Math.sin(radian));
+            locationHelper.zero().add(getCenter());
+            applyMechanics(ShapeDisplayMechanic.Phase.BEFORE_ROTATION, particle, locationHelper, vectorHelper);
+            rot.apply(vectorHelper);
+
+            for (ParticleShapeCompound compound : compounds) {
+                rotHelper.inherit(compound).apply(vectorHelper);
             }
 
-            for (int k = current; k < particleAmount; k++) {
-                Particle particle = getCurrentParticle();
-                double radian = Math.PI * 2 / particleAmount * k;
+            applyMechanics(ShapeDisplayMechanic.Phase.AFTER_ROTATION, particle, locationHelper, vectorHelper);
+            locationHelper.add(vectorHelper);
 
-                locationHelper.zero().add(getCenter());
-                vectorHelper.setX(xRadius * Math.sin(curveRadian) * Math.cos(radian));
-                vectorHelper.setY(yRadius * Math.cos(curveRadian));
-                vectorHelper.setZ(zRadius * Math.sin(curveRadian) * Math.sin(radian));
-                applyMechanics(ShapeDisplayMechanic.Phase.BEFORE_ROTATION, particle, locationHelper, vectorHelper);
-                rot.apply(vectorHelper);
-
-                for (ParticleShapeCompound compound : compounds) {
-                    rotHelper.inherit(compound).apply(vectorHelper);
-                }
-
-                applyMechanics(ShapeDisplayMechanic.Phase.AFTER_ROTATION, particle, locationHelper, vectorHelper);
-                locationHelper.add(vectorHelper);
-
-                if (!players.isEmpty()) {
-                    particle.displayForPlayers(locationHelper, players);
-                } else {
-                    particle.display(locationHelper);
-                }
-
-                overallCount++;
-
-                applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY, particle, locationHelper, vectorHelper);
-
-                if (trackCount) {
-                    currentCount++;
-                    hasRan = true;
-
-                    if (currentCount >= particlesPerDisplay) {
-                        currentCount = 0;
-
-                        break main;
-                    }
-                }
+            if (!players.isEmpty()) {
+                particle.displayForPlayers(locationHelper, players);
+            } else {
+                particle.display(locationHelper);
             }
 
-            current = 0;
+            overallCount++;
+
+            applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY, particle, locationHelper, vectorHelper);
+
+            if (trackCount) {
+                currentCount++;
+                hasRan = true;
+
+                if (currentCount >= particlesPerDisplay) {
+                    currentCount = 0;
+
+                    break;
+                }
+            }
         }
 
         if (!trackCount || !hasRan) {
@@ -151,7 +105,7 @@ public class ParticleSphere extends ParticleSphereSFSA {
 
     @Override
     public ParticleSphere clone() {
-        ParticleSphere clone = new ParticleSphere(particle, locations.get(0).clone(), xRadius, yRadius, zRadius, getPitch(), getYaw(), getRoll(), circleFrequency, particleFrequency);
+        ParticleSphere clone = new ParticleSphere(particle, locations.get(0).clone(), xRadius, yRadius, zRadius, getPitch(), getYaw(), getRoll(), particleFrequency);
 
         clone.rot.inherit(rot);
         clone.rot2.inherit(rot2);
@@ -169,71 +123,25 @@ public class ParticleSphere extends ParticleSphereSFSA {
         return clone;
     }
 
-    protected void recalcCircumferenceAndArea() {
-        cirTracker.clear();
-
-        surfaceArea = 0;
-
-        for (int i = 0; i < circleFrequency; i++) {
-            double curveRadian = (Math.PI - (Math.PI * limit / 100)) / (circleFrequency - 1) * i;
-
-            if (!limitInverse) {
-                curveRadian += Math.PI * limit / 100;
-            }
-
-            double curve = Math.sin(curveRadian);
-            double circumference;
-
-            if (xRadius == zRadius) {
-                circumference = Math.PI * 2 * Math.abs(xRadius * curve);
-            } else {
-                circumference = Math.PI * 2 * Math.sqrt((Math.pow(xRadius * curve, 2) + Math.pow(zRadius * curve, 2)) / 2);
-            }
-
-            cirTracker.add(circumference);
-
-            surfaceArea += circumference;
-        }
-
-        recalculate = false;
-    }
-
     @Override
-    public void setXRadius(double xRadius) {
-        super.setXRadius(xRadius);
-
-        recalculate = true;
-    }
-
-    @Override
-    public void setZRadius(double zRadius) {
-        super.setZRadius(zRadius);
-
-        recalculate = true;
-    }
-
-    @Override
-    public void setLimit(double limit) {
-        super.setLimit(limit);
-
-        recalculate = true;
+    public void scale(double x, double y, double z) {
+        super.scale(x, y, z);
+        setYRadius(getYRadius() * y);
     }
 
     /**
-     * @param circleFrequency amount of circles in the sphere
+     * @param limitInverse determines if the limit cuts off at the top or bottom. default false (top)
      */
-    public void setCircleFrequency(int circleFrequency) {
-        Validate.isTrue(circleFrequency >= 3, "You cant have a sphere with only 2 points!");
-        Validate.isTrue(circleFrequency <= particleFrequency, "You can't have more circles than particles!");
-
-        this.circleFrequency = circleFrequency;
-        recalculate = true;
+    @Override
+    public void setLimitInverse(boolean limitInverse) {
+        super.setLimitInverse(limitInverse);
     }
 
-    /**
-     * @return amount of circles in the sphere
-     */
-    public int getCircleFrequency() {
-        return circleFrequency;
+    public void setYRadius(double yRadius) {
+        this.yRadius = yRadius;
+    }
+
+    public double getYRadius() {
+        return yRadius;
     }
 }
