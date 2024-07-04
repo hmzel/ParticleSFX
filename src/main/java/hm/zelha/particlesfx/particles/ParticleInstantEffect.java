9ddfbc5cf11a93d4e5300ccf6be0a26de00559aa
hmzel <officiallucidus@gmail.com>
2024-07-04 09:06:05 -0500

    made it so the names of Particle classes match their in-game counterparts more accurately
    (cherry picked from commit 304a3c642eb48982a9771aff6ae6abc9a46f090f)

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleInstantEffect.java
@@ -0,0 +1,34 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_10_R1.EnumParticle;
+
+public class ParticleInstantEffect extends Particle {
+    public ParticleInstantEffect(double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.SPELL_INSTANT, offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleInstantEffect(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleInstantEffect(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleInstantEffect() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleInstantEffect inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleInstantEffect clone() {
+        return new ParticleInstantEffect().inherit(this);
+    }
+}
