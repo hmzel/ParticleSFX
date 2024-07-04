94112fff5f7212d69ab21f3c4ec9a4df4280b2f0
hmzel <officiallucidus@gmail.com>
2024-07-04 09:06:05 -0500

    made it so the names of Particle classes match their in-game counterparts more accurately
    (cherry picked from commit 304a3c642eb48982a9771aff6ae6abc9a46f090f)

--- src/main/java/hm/zelha/particlesfx/particles/ParticleSoulSculk.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleSoulSculk.java
@@ -5,56 +5,56 @@ import hm.zelha.particlesfx.particles.parents.TravellingParticle;
 import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
-public class ParticleSculkSoul extends TravellingParticle {
-    public ParticleSculkSoul(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
+public class ParticleSoulSculk extends TravellingParticle {
+    public ParticleSoulSculk(Location toGo, double offsetX, double offsetY, double offsetZ, int count) {
         super("sculk_soul", false, 0.06, null, toGo, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleSculkSoul(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
+    public ParticleSoulSculk(Vector velocity, double offsetX, double offsetY, double offsetZ, int count) {
         super("sculk_soul", false, 0.06, velocity, null, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleSculkSoul(Location toGo, double offsetX, double offsetY, double offsetZ) {
+    public ParticleSoulSculk(Location toGo, double offsetX, double offsetY, double offsetZ) {
         this(toGo, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleSculkSoul(Vector velocity, double offsetX, double offsetY, double offsetZ) {
+    public ParticleSoulSculk(Vector velocity, double offsetX, double offsetY, double offsetZ) {
         this(velocity, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleSculkSoul(double offsetX, double offsetY, double offsetZ, int count) {
+    public ParticleSoulSculk(double offsetX, double offsetY, double offsetZ, int count) {
         this((Location) null, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleSculkSoul(double offsetX, double offsetY, double offsetZ) {
+    public ParticleSoulSculk(double offsetX, double offsetY, double offsetZ) {
         this((Location) null, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleSculkSoul(int count) {
+    public ParticleSoulSculk(int count) {
         this((Location) null, 0, 0, 0, count);
     }
 
-    public ParticleSculkSoul(Location toGo) {
+    public ParticleSoulSculk(Location toGo) {
         this(toGo, 0, 0, 0, 1);
     }
 
-    public ParticleSculkSoul(Vector velocity) {
+    public ParticleSoulSculk(Vector velocity) {
         this(velocity, 0, 0, 0, 1);
     }
 
-    public ParticleSculkSoul() {
+    public ParticleSoulSculk() {
         this((Location) null, 0, 0, 0, 1);
     }
 
     @Override
-    public ParticleSculkSoul inherit(Particle particle) {
+    public ParticleSoulSculk inherit(Particle particle) {
         super.inherit(particle);
 
         return this;
     }
 
     @Override
-    public ParticleSculkSoul clone() {
-        return new ParticleSculkSoul().inherit(this);
+    public ParticleSoulSculk clone() {
+        return new ParticleSoulSculk().inherit(this);
     }
 }
