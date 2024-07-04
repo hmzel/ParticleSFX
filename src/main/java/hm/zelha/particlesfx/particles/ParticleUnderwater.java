6b26ebfaa46798e5acf715829ba1a3bf7216b781
hmzel <officiallucidus@gmail.com>
2024-07-04 09:06:05 -0500

    made it so the names of Particle classes match their in-game counterparts more accurately
    (cherry picked from commit 304a3c642eb48982a9771aff6ae6abc9a46f090f)

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleUnderwater.java
@@ -0,0 +1,41 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_15_R1.Particles;
+
+/**
+ * NOTE: only visible underwater
+ */
+public class ParticleUnderwater extends Particle {
+    /**@see ParticleUnderwater */
+    public ParticleUnderwater(double offsetX, double offsetY, double offsetZ, int count) {
+        super(Particles.UNDERWATER, offsetX, offsetY, offsetZ, count);
+    }
+
+    /**@see ParticleUnderwater */
+    public ParticleUnderwater(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    /**@see ParticleUnderwater */
+    public ParticleUnderwater(int count) {
+        this(0, 0, 0, count);
+    }
+
+    /**@see ParticleUnderwater */
+    public ParticleUnderwater() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleUnderwater inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleUnderwater clone() {
+        return new ParticleUnderwater().inherit(this);
+    }
+}
