93deb35ef1081daff5415cde4f9162d35de5a76d
hmzel <officiallucidus@gmail.com>
2024-07-17 23:22:03 -0500

    added AFTER_DISPLAY Phase to ShapeDisplayMechanic
    (cherry picked from commit 6b91160f9320433ce06dff85ef375e0f098f7c66)

--- src/main/java/hm/zelha/particlesfx/shapers/ParticleImage.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleImage.java
@@ -181,6 +181,8 @@ public class ParticleImage extends ParticleShaper {
             }
         }
 
+        applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY, particle, locationHelper, vectorHelper);
+
         if (!trackCount || !hasRan) {
             applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY_FULL, particle, locationHelper, vectorHelper);
 
--- src/main/java/hm/zelha/particlesfx/shapers/ParticleSpiral.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleSpiral.java
@@ -136,6 +136,8 @@ public class ParticleSpiral extends ParticleShaper {
             }
         }
 
+        applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY, particle, locationHelper, vectorHelper);
+
         if (!trackCount || !hasRan) {
             applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY_FULL, particle, locationHelper, vectorHelper);
 
