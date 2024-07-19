fb44c6626ec6e05984596d4efd5249278c52533b
hmzel <officiallucidus@gmail.com>
2024-07-18 23:10:16 -0500

    added javadocs to ShapeDisplayMechanic's Phase enum
    (cherry picked from commit a05200865ff48ca34e035f02c07a89b3f0d8cf1a)

--- src/main/java/hm/zelha/particlesfx/util/ShapeDisplayMechanic.java
+++ src/main/java/hm/zelha/particlesfx/util/ShapeDisplayMechanic.java
@@ -1,6 +1,7 @@
 package hm.zelha.particlesfx.util;
 
 import hm.zelha.particlesfx.particles.parents.Particle;
+import hm.zelha.particlesfx.shapers.ParticleCircle;
 import org.bukkit.Location;
 import org.bukkit.util.Vector;
 
@@ -16,12 +17,35 @@ public interface ShapeDisplayMechanic {
     void apply(Particle particle, Location current, Vector addition, int count);
 
     enum Phase {
+        /**
+         * Ran before any particles are displayed on every call of the display() method.
+         */
         BEFORE_DISPLAY,
+        /**
+         * Ran before any particles are displayed on every full display of the shape.
+         */
         BEFORE_DISPLAY_FULL,
+        /**
+         * Ran before rotation is applied to the location the particle will be displayed to. <br><br>
+         * Always ran before the particle is displayed regardless of if rotation is applied in this class or not.
+         */
         BEFORE_ROTATION,
+        /**
+         * Ran after rotation is applied to the location the particle will be displayed to. <br><br>
+         * Only ran if rotation is applied in this class. (ex. {@link ParticleCircle})
+         */
         AFTER_ROTATION,
+        /**
+         * Ran after a particle is displayed at the current location.
+         */
         AFTER_DISPLAY_PARTICLE,
+        /**
+         * Ran after all particles are displayed on every call of the display() method.
+         */
         AFTER_DISPLAY,
+        /**
+         * Ran after all particles are displayed on every full display of the shape.
+         */
         AFTER_DISPLAY_FULL
     }
 }
