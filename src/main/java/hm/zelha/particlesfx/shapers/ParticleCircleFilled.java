1b28bca9c89388e1244b5ae73a25b3646d298139
hmzel <officiallucidus@gmail.com>
2024-07-19 00:02:31 -0500

    made it so ParticleShape's display is reset if it wasnt able to display all particles, previously it only reset if it displayed none
    (cherry picked from commit de9f9f92e6ec4fb1e9e22568abd5b94181c17c48)

--- src/main/java/hm/zelha/particlesfx/shapers/ParticleCircleFilled.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleCircleFilled.java
@@ -79,14 +79,11 @@ public class ParticleCircleFilled extends ParticleCircle {
 
             applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY_PARTICLE, particle, locationHelper, vectorHelper);
 
-            if (trackCount) {
+            if (trackCount && currentCount >= particlesPerDisplay) {
+                currentCount = 0;
                 hasRan = true;
 
-                if (currentCount >= particlesPerDisplay) {
-                    currentCount = 0;
-
-                    break;
-                }
+                break;
             }
         }
 
--- src/main/java/hm/zelha/particlesfx/shapers/ParticleText.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleText.java
@@ -123,14 +123,11 @@ public class ParticleText extends ParticleShaper {
 
             applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY_PARTICLE, particle, locationHelper, vectorHelper);
 
-            if (trackCount) {
+            if (trackCount && currentCount >= particlesPerDisplay) {
+                currentCount = 0;
                 hasRan = true;
 
-                if (currentCount >= particlesPerDisplay) {
-                    currentCount = 0;
-
-                    break;
-                }
+                break;
             }
         }
 
