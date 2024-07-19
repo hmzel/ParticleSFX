03f8d290c9f340f703ba952d596e550f8857f26c
hmzel <officiallucidus@gmail.com>
2024-07-19 02:39:02 -0500

    made it so ParticleShape displays dont just stop when they reach the end of the shape and havent displayed the amount set by particlesPerDisplay
    (cherry picked from commit 77a2c840502cd527237bd7acb72a9036cc370b9a)

--- src/main/java/hm/zelha/particlesfx/shapers/ParticleText.java
+++ src/main/java/hm/zelha/particlesfx/shapers/ParticleText.java
@@ -131,12 +131,21 @@ public class ParticleText extends ParticleShaper {
             }
         }
 
-        applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY, particle, locationHelper, vectorHelper);
+        if (!trackCount || hasRan) {
+            applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY, particle, locationHelper, vectorHelper);
+        }
 
         if (!trackCount || !hasRan) {
             applyMechanics(ShapeDisplayMechanic.Phase.AFTER_DISPLAY_FULL, particle, locationHelper, vectorHelper);
 
             overallCount = 0;
+
+            if (trackCount) {
+                display();
+
+                return;
+            }
+
             currentCount = 0;
         }
     }
