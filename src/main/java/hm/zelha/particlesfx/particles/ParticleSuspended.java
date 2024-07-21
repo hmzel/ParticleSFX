815cd1598eadb3eaf23ceb8bdf66aa85ae372b4e
hmzel <officiallucidus@gmail.com>
2024-07-20 22:16:40 -0500

    made it so the names of Particle classes match their in-game counterparts more accurately part 2

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleSuspended.java
@@ -0,0 +1,41 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.Particle;
+import net.minecraft.server.v1_11_R1.EnumParticle;
+
+/**
+ * NOTE: only visible underwater
+ */
+public class ParticleSuspended extends Particle {
+    /**@see ParticleSuspended */
+    public ParticleSuspended(double offsetX, double offsetY, double offsetZ, int count) {
+        super(EnumParticle.SUSPENDED, offsetX, offsetY, offsetZ, count);
+    }
+
+    /**@see ParticleSuspended */
+    public ParticleSuspended(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    /**@see ParticleSuspended */
+    public ParticleSuspended(int count) {
+        this(0, 0, 0, count);
+    }
+
+    /**@see ParticleSuspended */
+    public ParticleSuspended() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleSuspended inherit(Particle particle) {
+        super.inherit(particle);
+
+        return this;
+    }
+
+    @Override
+    public ParticleSuspended clone() {
+        return new ParticleSuspended().inherit(this);
+    }
+}
