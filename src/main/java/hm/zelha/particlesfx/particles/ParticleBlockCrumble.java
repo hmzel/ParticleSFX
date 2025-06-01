663273ad4828650924857c24a39c8c1ba72bb070
hmzel <hmzeldev@gmail.com>
2025-05-31 22:25:25 -0500

    added particle class ParticleBlockCrumble

--- src/main/java/hm/zelha/particlesfx/particles/ParticleBlockCrumble.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleBlockCrumble.java
@@ -10,43 +10,43 @@ import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_21_R2.block.data.CraftBlockData;
 import org.bukkit.craftbukkit.v1_21_R2.util.CraftMagicNumbers;
 
-public class ParticleBlockBreak extends Particle implements MaterialParticle {
-    public ParticleBlockBreak(Material material, double offsetX, double offsetY, double offsetZ, int count) {
+public class ParticleBlockCrumble extends Particle implements MaterialParticle {
+    public ParticleBlockCrumble(Material material, double offsetX, double offsetY, double offsetZ, int count) {
         super("", offsetX, offsetY, offsetZ, count);
 
         setMaterial(material);
     }
 
-    public ParticleBlockBreak(double offsetX, double offsetY, double offsetZ, int count) {
+    public ParticleBlockCrumble(double offsetX, double offsetY, double offsetZ, int count) {
         this(Material.DRAGON_EGG, offsetX, offsetY, offsetZ, count);
     }
 
-    public ParticleBlockBreak(Material material, double offsetX, double offsetY, double offsetZ) {
+    public ParticleBlockCrumble(Material material, double offsetX, double offsetY, double offsetZ) {
         this(material, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleBlockBreak(double offsetX, double offsetY, double offsetZ) {
+    public ParticleBlockCrumble(double offsetX, double offsetY, double offsetZ) {
         this(Material.DRAGON_EGG, offsetX, offsetY, offsetZ, 1);
     }
 
-    public ParticleBlockBreak(Material material, int count) {
+    public ParticleBlockCrumble(Material material, int count) {
         this(material, 0, 0, 0, count);
     }
 
-    public ParticleBlockBreak(Material material) {
+    public ParticleBlockCrumble(Material material) {
         this(material, 0, 0, 0, 1);
     }
 
-    public ParticleBlockBreak(int count) {
+    public ParticleBlockCrumble(int count) {
         this(Material.DRAGON_EGG, 0, 0, 0, count);
     }
 
-    public ParticleBlockBreak() {
+    public ParticleBlockCrumble() {
         this(Material.DRAGON_EGG, 0, 0, 0, 1);
     }
 
     @Override
-    public ParticleBlockBreak inherit(Particle particle) {
+    public ParticleBlockCrumble inherit(Particle particle) {
         super.inherit(particle);
 
         if (particle instanceof MaterialParticle) {
@@ -57,15 +57,15 @@ public class ParticleBlockBreak extends Particle implements MaterialParticle {
     }
 
     @Override
-    public ParticleBlockBreak clone() {
-        return new ParticleBlockBreak().inherit(this);
+    public ParticleBlockCrumble clone() {
+        return new ParticleBlockCrumble().inherit(this);
     }
 
     public void setMaterial(Material material) {
         Validate.notNull(material, "Material cannot be null!");
         Validate.isTrue(material.isBlock(), "Material must be a block!");
 
-        particle = new ParticleParamBlock((net.minecraft.core.particles.Particle) BuiltInRegistries.i.a(MinecraftKey.a("minecraft", "block")), ((CraftBlockData) material.createBlockData()).getState());
+        particle = new ParticleParamBlock((net.minecraft.core.particles.Particle) BuiltInRegistries.i.a(MinecraftKey.a("minecraft", "block_crumble")), ((CraftBlockData) material.createBlockData()).getState());
     }
 
     public Material getMaterial() {
