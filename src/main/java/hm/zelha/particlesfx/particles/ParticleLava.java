d805324164f684aa623ed759cd0f2a04efacc32b
hmzel <hmzeldev@gmail.com>
2025-06-07 01:29:38 -0500

    Combined all of the different dripping, landing, and falling particle variants into a singular particle class, using a LiquidParticle interface to set which state of the particle to use

---
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleLava.java
@@ -0,0 +1,60 @@
+package hm.zelha.particlesfx.particles;
+
+import hm.zelha.particlesfx.particles.parents.LiquidParticle;
+import hm.zelha.particlesfx.particles.parents.Particle;
+import hm.zelha.particlesfx.util.LiquidParticleState;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.MinecraftKey;
+
+import java.util.Locale;
+
+public class ParticleLava extends Particle implements LiquidParticle {
+
+    private LiquidParticleState state = LiquidParticleState.DRIPPING;
+
+    public ParticleLava(double offsetX, double offsetY, double offsetZ, int count) {
+        super("dripping_lava", offsetX, offsetY, offsetZ, count);
+    }
+
+    public ParticleLava(double offsetX, double offsetY, double offsetZ) {
+        this(offsetX, offsetY, offsetZ, 1);
+    }
+
+    public ParticleLava(int count) {
+        this(0, 0, 0, count);
+    }
+
+    public ParticleLava() {
+        this(0, 0, 0, 1);
+    }
+
+    @Override
+    public ParticleLava inherit(Particle particle) {
+        super.inherit(particle);
+
+        if (particle instanceof LiquidParticle) {
+            ((LiquidParticle) particle).setLiquidState(getLiquidState());
+        }
+
+        return this;
+    }
+
+    @Override
+    public ParticleLava clone() {
+        return new ParticleLava().inherit(this);
+    }
+
+    @Override
+    public ParticleLava setLiquidState(LiquidParticleState state) {
+        particle = (ParticleType) BuiltInRegistries.i.a(MinecraftKey.a("minecraft", state.name().toLowerCase(Locale.ROOT) + "_lava"));
+        this.state = state;
+
+        return this;
+    }
+
+    @Override
+    public LiquidParticleState getLiquidState() {
+        return state;
+    }
+}
