01c487d160959f981f763119bd6ee5b24091bba8
hmzel <hmzeldev@gmail.com>
2025-06-08 10:33:34 -0500

    added IParticle interface to reduce duplicate code in parent particle classes

--- src/main/java/hm/zelha/particlesfx/particles/parents/Particle.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/Particle.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
-public abstract class Particle {
+public abstract class Particle implements IParticle {
 
     protected final ThreadLocalRandom rng = ThreadLocalRandom.current();
     protected final Vector fakeOffsetHelper = new Vector();
@@ -73,10 +73,6 @@ public abstract class Particle {
         display(location, listHelper);
     }
 
-    /**
-     * @param particle particle for this object to copy data from
-     * @return this object
-     */
     public Particle inherit(Particle particle) {
         offsetX = particle.offsetX;
         offsetY = particle.offsetY;
