b14bc53265605df279b0748ea9ea4d4133aa174f
hmzel <hmzeldev@gmail.com>
2025-06-08 10:33:34 -0500

    added IParticle interface to reduce duplicate code in parent particle classes

--- src/main/java/hm/zelha/particlesfx/particles/parents/MaterialParticle.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/MaterialParticle.java
@@ -1,54 +1,17 @@
 package hm.zelha.particlesfx.particles.parents;
 
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Player;
 
-import java.util.List;
-import java.util.UUID;
-
-public interface MaterialParticle {
-    void display(Location location);
-
-    void displayForPlayers(Location location, Player... players);
-
-    void displayForPlayers(Location location, List<UUID> players);
-
-    /**
-     * @param particle particle for this object to copy data from
-     * @return this object
-     */
+public interface MaterialParticle extends IParticle {
     MaterialParticle inherit(Particle particle);
 
     MaterialParticle clone();
 
     void setMaterial(Material material);
 
-    void setOffset(double x, double y, double z);
-
-    void setOffsetX(double offsetX);
-
-    void setOffsetY(double offsetY);
-
-    void setOffsetZ(double offsetZ);
-
     Particle setSpeed(double speed);
-
-    void setCount(int count);
 
     Particle setRadius(int radius);
 
     Material getMaterial();
-
-    double getOffsetX();
-
-    double getOffsetY();
-
-    double getOffsetZ();
-
-    double getSpeed();
-
-    int getCount();
-
-    int getRadius();
 }
