bef3c2bf3ec70116f1094f1cbb6d702f26228e95
hmzel <hmzeldev@gmail.com>
2025-06-08 10:33:34 -0500

    added IParticle interface to reduce duplicate code in parent particle classes

--- src/main/java/hm/zelha/particlesfx/particles/parents/SizeableParticle.java
+++ src/main/java/hm/zelha/particlesfx/particles/parents/SizeableParticle.java
@@ -1,53 +1,15 @@
 package hm.zelha.particlesfx.particles.parents;
 
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
-
-import java.util.List;
-import java.util.UUID;
-
-public interface SizeableParticle {
-    void display(Location location);
-
-    void displayForPlayers(Location location, Player... players);
-
-    void displayForPlayers(Location location, List<UUID> players);
-
-    /**
-     * @param particle particle for this object to copy data from
-     * @return this object
-     */
+public interface SizeableParticle extends IParticle {
     SizeableParticle inherit(Particle particle);
 
     SizeableParticle clone();
 
     void setSize(double size);
 
-    void setOffset(double x, double y, double z);
-
-    void setOffsetX(double offsetX);
-
-    void setOffsetY(double offsetY);
-
-    void setOffsetZ(double offsetZ);
-
     Particle setSpeed(double speed);
-
-    void setCount(int count);
 
     Particle setRadius(int radius);
 
     double getSize();
-
-    double getOffsetX();
-
-    double getOffsetY();
-
-    double getOffsetZ();
-
-    double getSpeed();
-
-    int getCount();
-
-    int getRadius();
 }
