edea007e4dddcfe1606d331f1dbd68ae993e9d5c
hmzel <hmzeldev@gmail.com>
2025-06-10 03:24:49 -0500

    fixed LiquidParticles

--- src/main/java/hm/zelha/particlesfx/particles/ParticleNectar.java
+++ src/main/java/hm/zelha/particlesfx/particles/ParticleNectar.java
@@ -50,7 +50,7 @@ public class ParticleNectar extends Particle implements LiquidParticle {
         if (state == LiquidParticleState.DRIPPING) throw new IllegalArgumentException("The \"DRIPPING\" state doesn't exist for this particle!");
         if (state == LiquidParticleState.LANDING) throw new IllegalArgumentException("The \"LANDING\" state doesn't exist for this particle!");
 
-        particle = (ParticleType) IRegistry.aa.a(new MinecraftKey(state.name().toLowerCase(Locale.ROOT) + "_nectar"));
+        particle = (ParticleType) IRegistry.Z.a(new MinecraftKey(state.name().toLowerCase(Locale.ROOT) + "_nectar"));
         this.state = state;
 
         return this;
